7dfc03f54353685b620f2b8c967107250ec28fc0
zacum <mfilgyhty@hotmail.com>
2017-02-17 16:04:24 -0500

    small imprv too login screen

--- client/app/pages/auth/auth.module.ts
+++ client/app/pages/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login';
 import { LogoutComponent } from './logout';
@@ -10,7 +11,7 @@ import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [ LoginComponent, LogoutComponent ],
-  imports: [ BrowserModule, ReactiveFormsModule, SharedModule ],
+  imports: [ BrowserModule, ReactiveFormsModule, SharedModule, RouterModule ],
   providers: [ AuthService ],
 })
 export class AuthModule {}
