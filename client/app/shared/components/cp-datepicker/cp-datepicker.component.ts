2879eff5e4015d6da7e3a1e09ad0a2732bd8ae76
zacum <mfilgyhty@hotmail.com>
2017-02-17 11:40:01 -0500

    create datepicker component

---
+++ client/app/shared/components/cp-datepicker/cp-datepicker.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cp-datepicker',
+  templateUrl: './cp-datepicker.component.html',
+  styleUrls: ['./cp-datepicker.component.scss']
+})
+export class CPDatePickerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ client/app/shared/components/cp-datepicker/index.ts
@@ -0,0 +1,1 @@
+export * from './cp-datepicker.component';
