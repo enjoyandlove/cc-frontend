f023ec2acc221aee90bbf23bf3b816bc8cb3e3f3
zacum <mfilgyhty@hotmail.com>
2017-03-14 14:49:49 -0400

    disabled submit button if mandatory fields are not present

--- client/app/pages/controlpanel/manage/team/create/team-create.component.ts
+++ client/app/pages/controlpanel/manage/team/create/team-create.component.ts
@@ -1,4 +1,10 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { STATUS } from '../../../../../shared/constants';
+import { ErrorService } from '../../../../../shared/services';
+import { HEADER_UPDATE, IHeader } from '../../../../../reducers/header.reducer';
 
 @Component({
   selector: 'cp-team-create',
@@ -6,7 +12,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-create.component.scss']
 })
 export class TeamCreateComponent implements OnInit {
-  constructor() { }
+  form: FormGroup;
+  isAllAccessEnabled;
 
-  ngOnInit() { }
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<IHeader>,
+    private errorService: ErrorService
+  ) { }
+
+  private buildHeader() {
+    this.store.dispatch({
+      type: HEADER_UPDATE,
+      payload: require('../team.header.json')
+    });
+  }
+
+  private buildForm() {
+    this.form = this.fb.group({
+      'first_name': ['', Validators.required],
+      'last_name': ['', Validators.required],
+      'email': ['', Validators.required],
+    });
+  }
+
+  onSubmit(data) {
+    console.log(data);
+    if (!this.form.valid) {
+      this.errorService.handleError({ reason: STATUS.ALL_FIELDS_ARE_REQUIRED });
+      return;
+    }
+
+    console.log(data);
+  }
+
+  ngOnInit() {
+    this.buildHeader();
+    this.buildForm();
+  }
 }
