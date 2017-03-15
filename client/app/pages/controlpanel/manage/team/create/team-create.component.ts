28caf3d089c0411770885e4b132f90af67c73f22
zacum <mfilgyhty@hotmail.com>
2017-03-15 11:19:52 -0400

    latest changes from old machine

--- client/app/pages/controlpanel/manage/team/create/team-create.component.ts
+++ client/app/pages/controlpanel/manage/team/create/team-create.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
@@ -35,18 +35,93 @@ export class TeamCreateComponent implements OnInit {
     this.form = this.fb.group({
       'first_name': ['', Validators.required],
       'last_name': ['', Validators.required],
-      'email': ['', Validators.required],
+      'email': ['', Validators.required]
     });
+
+    this.buildFormGroupsByPermission();
   }
 
   onSubmit(data) {
     console.log(data);
+    console.log(this.form);
     if (!this.form.valid) {
       this.errorService.handleError({ reason: STATUS.ALL_FIELDS_ARE_REQUIRED });
       return;
     }
 
-    console.log(data);
+    // console.log(data);
+  }
+
+  toggleAllAccess(checked) {
+    let value = checked ? 1 : 2;
+
+    let clubsControl = <FormArray>this.form.controls['clubs'];
+    let eventsControl = <FormArray>this.form.controls['events'];
+    let serviceControl = <FormArray>this.form.controls['services'];
+
+    clubsControl.controls['clubs'].setValue(value);
+    eventsControl.controls['events'].setValue(value);
+    serviceControl.controls['services'].setValue(value);
+  }
+
+  buildServicesGroup(): void {
+    let group = this.fb.group({
+      'services': [1, Validators.required]
+    });
+    this.form.registerControl('services', group);
+  }
+
+  buildClubsGroup(): void {
+    let group = this.fb.group({
+      'clubs': [1, Validators.required]
+    });
+    this.form.registerControl('clubs', group);
+  }
+
+  buildEventsGroup(): void {
+    let group = this.fb.group({
+      'events': [1, Validators.required]
+    });
+    this.form.registerControl('events', group);
+  }
+
+  buildContentGroup(): void {
+    let group = this.fb.group({
+      'orientation': [false, Validators.required],
+      'calendars': [false, Validators.required],
+      'maps': [false, Validators.required],
+      'feeds': [false, Validators.required],
+      'links': [false, Validators.required],
+      'app_customization': [false, Validators.required]
+    });
+    this.form.registerControl('content', group);
+  }
+
+  buildNotificationsGroup(): void {
+    let group = this.fb.group({
+      'campus': [false, Validators.required],
+      'emergency': [false, Validators.required]
+    });
+    this.form.registerControl('notifications', group);
+  }
+
+  checkControl(checked, group, controlName): void {
+    let control = <FormArray>this.form.controls[group];
+    control.controls[controlName].setValue(checked);
+  }
+
+  buildFormGroupsByPermission() {
+    /**
+     * check user permissions and
+     * build form groups if user has
+     * access to that section
+     */
+
+    this.buildServicesGroup();
+    this.buildClubsGroup();
+    this.buildEventsGroup();
+    this.buildContentGroup();
+    this.buildNotificationsGroup();
   }
 
   ngOnInit() {
