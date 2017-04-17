55f78759994ae82ce77727cfcfa098bfcffe56e9
zacum <mfilgyhty@hotmail.com>
2017-04-17 10:11:43 -0400

    latest changes for cover photo

--- client/app/pages/controlpanel/manage/customization/list/customization-list.component.ts
+++ client/app/pages/controlpanel/manage/customization/list/customization-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Headers } from '@angular/http';
+
+import { API } from '../../../.././../config/api';
+import { STATUS } from '../../../.././../shared/constants';
+import { FileUploadService } from '../../../../../shared/services';
+import { CPArray, CPImage, appStorage } from '.././../../../../shared/utils';
 
 @Component({
   selector: 'cp-customization-list',
@@ -8,9 +14,12 @@ import { Component, OnInit } from '@angular/core';
 export class CustomizationListComponent implements OnInit {
   error;
   image;
+  canvas;
   isError;
 
-  constructor() { }
+  constructor(
+    private fileUploadService: FileUploadService
+  ) { }
 
   onError(error) {
     this.isError = true;
@@ -26,5 +35,77 @@ export class CustomizationListComponent implements OnInit {
     this.image = image;
   }
 
-  ngOnInit() { }
+  onSave() {
+    let promise: Promise<any> = this.canvas.result(
+      {
+        'type': 'blob',
+        'size': 'viewport',
+        'format': 'jpeg'
+      }
+    );
+
+    promise
+      .then(res => {
+        console.log(res);
+        console.log(typeof res);
+        // let reader = new FileReader();
+
+        // console.log(reader);
+
+        // reader.readAsDataURL(res);
+
+        // console.log(reader.result);
+
+        // // this.onFileUpload(res);
+      })
+      .catch(err => console.log(err));
+  }
+
+    onFileUpload(file) {
+    // this.reset.emit();
+
+    // if (!file) { return; }
+
+    // const fileExtension = CPArray.last(file.name.split('.'));
+
+    // if (!CPImage.isSizeOk(file.size, CPImage.MAX_IMAGE_SIZE)) {
+    //   this.error.emit(STATUS.FILE_IS_TOO_BIG);
+    //   return;
+    // }
+
+    // if (!CPImage.isValidExtension(fileExtension, CPImage.VALID_EXTENSIONS)) {
+    //   this.error.emit(STATUS.WRONG_EXTENSION);
+    //   return;
+    // }
+
+    const headers = new Headers();
+    const url = `${API.BASE_URL}/${API.VERSION.V1}/${API.ENDPOINTS.IMAGE}/`;
+    const auth = `${API.AUTH_HEADER.SESSION} ${appStorage.get(appStorage.keys.SESSION)}`;
+
+    headers.append('Authorization', auth);
+
+    this
+      .fileUploadService
+      .uploadFile(file, url, headers)
+      .subscribe(
+      res => console.log(res),
+      err => console.log(err)
+      );
+  }
+
+  onCancel() {
+    console.log('Cancel');
+  }
+
+  ngOnInit() {
+    // https://foliotek.github.io/Croppie/
+    let Croppie = require('croppie');
+
+    this.canvas = new Croppie(document.getElementById('canvas_wrapper'), {
+      'showZoomer': false,
+      'viewport': { width: 700, height: 270 },
+      'boundary': { height: 270 },
+      'url': 'https://source.unsplash.com/random/900x500'
+    });
+  }
 }
