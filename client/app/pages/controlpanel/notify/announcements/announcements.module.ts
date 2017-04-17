b36fe3fcd473a8397dce1800c7cae2de269adee3
zacum <mfilgyhty@hotmail.com>
2017-04-17 15:05:21 -0400

    GET

--- client/app/pages/controlpanel/notify/announcements/announcements.module.ts
+++ client/app/pages/controlpanel/notify/announcements/announcements.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../../../shared/shared.module';
 
-import { AnnouncementsListComponent }  from './list';
+import { AnnouncementsListComponent } from './list';
+
+import { AnnouncementsListActionBoxComponent
+} from './list/components';
 
 import { AnnouncementsRoutingModule } from './announcements.routing.module';
 
+import { AnnouncementsService } from './announcements.service';
+
 @NgModule({
-  declarations: [ AnnouncementsListComponent ],
+  declarations: [ AnnouncementsListComponent, AnnouncementsListActionBoxComponent ],
 
   imports: [ CommonModule, SharedModule, AnnouncementsRoutingModule ],
 
-  providers: [ ],
+  providers: [ AnnouncementsService ],
 })
 export class AnnouncementsModule {}
--- client/app/pages/controlpanel/notify/announcements/list/announcements-list.component.ts
+++ client/app/pages/controlpanel/notify/announcements/list/announcements-list.component.ts
@@ -1,12 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { URLSearchParams } from '@angular/http';
+
+import { AnnouncementsService } from '../announcements.service';
+import { BaseComponent } from '../../../../../base/base.component';
+
+import { FORMAT } from '../../../../../shared/pipes/date.pipe'
+
+interface IState {
+  messages: Array<any>;
+  query: string;
+  type: number;
+}
+
+const state: IState = {
+  messages: [],
+  query: null,
+  type: null
+};
+
+declare var $: any;
 
 @Component({
   selector: 'cp-announcements-list',
   templateUrl: './announcements-list.component.html',
   styleUrls: ['./announcements-list.component.scss']
 })
-export class AnnouncementsListComponent implements OnInit {
-  constructor() { }
+export class AnnouncementsListComponent extends BaseComponent implements OnInit {
+  loading;
+  state: IState = state;
+  dateFormat = FORMAT.DATETIME;
+
+  constructor(
+    private service: AnnouncementsService
+  ) {
+    super();
+    super.isLoading().subscribe(res => this.loading = res);
+
+    this.fetch();
+  }
+
+  doFilter(data) {
+    this.state = Object.assign(
+      {},
+      this.state,
+      {
+        query: data.query,
+        type: data.query
+      }
+    );
+
+    this.fetch();
+  }
+
+  private fetch() {
+    let search = new URLSearchParams();
+    let type = this.state.type ? this.state.type.toString() : null;
+
+    search.append('type', type);
+    search.append('query', this.state.query);
+
+    super
+      .fetchData(this.service.getAnnouncements(search))
+      .then(res => {
+        this.state = Object.assign({}, this.state, { messages: res.data });
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
 
   ngOnInit() { }
 }
