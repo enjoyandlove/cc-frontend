4cfd785dab17c597918fc8a02b8128fb3eb71a0a
zacum <mfilgyhty@hotmail.com>
2017-05-02 11:37:12 -0400

    clean up

--- client/app/config/guards/auth.guard.ts
+++ client/app/config/guards/auth.guard.ts
@@ -1,18 +1,44 @@
 /**
  * Guard to check if user is authenticated
  */
-import { CanActivate  } from '@angular/router';
-import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { CanActivate } from '@angular/router';
 
+import { CPSession } from '../../session';
 import { appStorage } from '../../shared/utils';
+import { AdminService, SchoolService } from '../../shared/services';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private session: CPSession,
+    private adminService: AdminService,
+    private schoolService: SchoolService
+  ) { }
 
   canActivate() {
-    if (appStorage.get(appStorage.keys.SESSION)) { return true; }
+    if (appStorage.get(appStorage.keys.SESSION)) {
+      const admins$ = this.adminService.getAdmins(1, 1);
+      const school$ = this.schoolService.getShool();
+      const stream$ = Observable.combineLatest(admins$, school$);
+
+      if (!this.session.school || !this.session.user) {
+        console.log('no school or user data');
+        return stream$
+          .toPromise()
+          .then(res => {
+            this.session.user = res[0][0];
+            this.session.school = res[1][0];
+            return true;
+          })
+          .catch(_ => false);
+      }
+      console.log('school or user data exists');
+      return true;
+    }
 
     this.router.navigate(['/login']);
     return false;
--- client/app/pages/controlpanel/controlpanel.component.ts
+++ client/app/pages/controlpanel/controlpanel.component.ts
@@ -1,52 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { CPSession } from '../../session';
-import { BaseComponent } from '../../base';
-import { AdminService, SchoolService } from '../../shared/services';
 
 @Component({
   selector: 'cp-controlpanel',
   styleUrls: ['./controlpanel.component.scss'],
   templateUrl: './controlpanel.component.html',
 })
-export class ControlPanelComponent extends BaseComponent implements OnInit {
-  status;
-  loading = true;
-
-  constructor(
-    private session: CPSession,
-    private adminService: AdminService,
-    private schoolService: SchoolService
-  ) {
-    super();
-
-    super.isLoading().subscribe(res => this.loading = res);
-
-    // if (!appStorage.get(appStorage.keys.PROFILE)) {
-    //   this.fetch();
-    // }
-  }
-
-  private fetch() {
-    const admins$ = this.adminService.getAdmins(1, 1);
-    const school$ = this.schoolService.getShool();
-    const stream$ = Observable.combineLatest(admins$, school$);
-
-    super
-      .fetchData(stream$)
-      .then(res => {
-        this.session.user = res.data[0][0];
-        this.session.school = res.data[1][0];
-        // appStorage.set(appStorage.keys.PROFILE, JSON.stringify(res.data[0][0]));
-      })
-      .catch(err => console.error(err));
-  }
+export class ControlPanelComponent implements OnInit {
+  constructor() { }
 
   ngOnInit() {
-    this.fetch();
-    // if (!appStorage.get(appStorage.keys.PROFILE)) {
-    //   this.fetch();
-    // }
+    console.log('control panel component init');
   }
 }
