ee6c302fcb733db67544f49b1c967027d36b84e3
zacum <mfilgyhty@hotmail.com>
2017-05-10 17:10:02 -0400

    revert to format address

--- client/app/pages/controlpanel/manage/locations/create/locations-create.component.ts
+++ client/app/pages/controlpanel/manage/locations/create/locations-create.component.ts
@@ -26,6 +26,7 @@ export class LocationsCreateComponent implements OnInit {
   ) { }
 
   onPlaceChange(data) {
+    console.log(data);
     let cpMap = CPMap.getBaseMapObject(data);
 
     this.form.controls['city'].setValue(cpMap.city);
@@ -33,7 +34,7 @@ export class LocationsCreateComponent implements OnInit {
     this.form.controls['country'].setValue(cpMap.country);
     this.form.controls['latitude'].setValue(cpMap.latitude);
     this.form.controls['longitude'].setValue(cpMap.longitude);
-    this.form.controls['address'].setValue(data.name);
+    this.form.controls['address'].setValue(data. formatted_address);
     this.form.controls['postal_code'].setValue(cpMap.postal_code);
 
     this.mapCenter.next(data.geometry.location.toJSON());
