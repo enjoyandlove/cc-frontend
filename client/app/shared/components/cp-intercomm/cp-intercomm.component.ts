0f19996d0b4e246ffcd84615f7e55b65b4965569
zacum <mfilgyhty@hotmail.com>
2017-05-18 12:18:46 -0400

    create component

---
+++ client/app/shared/components/cp-intercomm/cp-intercomm.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cp-intercomm',
+  templateUrl: './cp-intercomm.component.html',
+})
+export class CPIntercommComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
