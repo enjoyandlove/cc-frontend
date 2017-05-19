a8525c44c1f69c1642cfb220b205d5eab66dc14f
zacum <mfilgyhty@hotmail.com>
2017-05-19 17:26:38 -0400

    kill all session

--- client/app/pages/auth/logout/logout.component.ts
+++ client/app/pages/auth/logout/logout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { CPSession } from '../../../session';
 import { appStorage } from '../../../shared/utils';
 
 @Component({
@@ -8,9 +9,15 @@ import { appStorage } from '../../../shared/utils';
   template: '',
 })
 export class LogoutComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private session: CPSession
+  ) { }
 
   ngOnInit() {
+    this.session.user = null;
+    this.session.schools = [];
+    this.session.school = null;
     appStorage.clear();
     this.router.navigate(['/login']);
   }
