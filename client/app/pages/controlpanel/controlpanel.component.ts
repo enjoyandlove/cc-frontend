dfd451c3e64a26c6a342ac6e6c492e78a52e684a
zacum <mfilgyhty@hotmail.com>
2017-05-25 13:11:51 -0400

    add hotjar

--- client/app/pages/controlpanel/controlpanel.component.ts
+++ client/app/pages/controlpanel/controlpanel.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { isProd } from '../../config/env';
+
 @Component({
   selector: 'cp-controlpanel',
   styleUrls: ['./controlpanel.component.scss'],
   templateUrl: './controlpanel.component.html',
 })
 export class ControlPanelComponent implements OnInit {
+  isProd = isProd;
   constructor() { }
 
   ngOnInit() { }
