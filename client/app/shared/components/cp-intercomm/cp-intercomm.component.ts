716fcc67edcd0806eec28366f78ef38281a19faa
zacum <mfilgyhty@hotmail.com>
2017-05-25 14:28:49 -0400

    pass email to intercom

--- client/app/shared/components/cp-intercomm/cp-intercomm.component.ts
+++ client/app/shared/components/cp-intercomm/cp-intercomm.component.ts
@@ -2,16 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 declare var window: any;
 
+import { CPSession } from '../../../session';
+
 @Component({
   selector: 'cp-intercomm',
   templateUrl: './cp-intercomm.component.html',
 })
 export class CPIntercommComponent implements OnInit {
-  constructor() { }
+  constructor(
+    private session: CPSession
+  ) { }
 
   ngOnInit() {
     window.Intercom('boot', {
-      app_id: 'v0k6hr06'
+      app_id: 'v0k6hr06',
+      user_id: this.session.user.id,
+      email: this.session.user.email,
     });
   }
 }
