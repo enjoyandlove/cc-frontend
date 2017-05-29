9de29057aa80823814b50f1f39a8312d78ea5557
zacum <mfilgyhty@hotmail.com>
2017-05-29 11:01:23 -0400

    redirect to web_app

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from controlpanel import views
 
 urlpatterns = [
     url(r'^', include('controlpanel.urls', namespace="controlpanel")),
+    url(r'^', views.handle_404)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
