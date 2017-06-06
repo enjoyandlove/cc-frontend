d37758bc83f4527d8b1a11d92e9b20ab47b5baaf
zacum <mfilgyhty@hotmail.com>
2017-06-06 10:22:15 -0400

    fix clubs event header

--- client/app/pages/controlpanel/manage/clubs/events/clubs-events.component.ts
+++ client/app/pages/controlpanel/manage/clubs/events/clubs-events.component.ts
@@ -1,20 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { URLSearchParams } from '@angular/http';
+import { Store } from '@ngrx/store';
+
+import { ClubsService } from '../clubs.service';
+import { CPSession } from '../../../../../session';
+import { BaseComponent } from '../../../../../base/base.component';
+import { HEADER_UPDATE } from '../../../../../reducers/header.reducer';
+import { CP_PRIVILEGES_MAP } from '../../../../../shared/utils/privileges';
+
 
 @Component({
   selector: 'cp-clubs-events',
   template: '<cp-events [isClub]="isClub" [clubId]="clubId"></cp-events>'
 })
-export class ClubsEventsComponent implements OnInit {
-  clubId: number;
+export class ClubsEventsComponent extends BaseComponent implements OnInit {
+  loading;
   isClub = true;
+  hasMembership;
+  clubId: number;
 
   constructor(
+    private store: Store<any>,
+    private session: CPSession,
     private route: ActivatedRoute,
+    private clubsService: ClubsService
   ) {
+    super();
     this.clubId = this.route.parent.snapshot.parent.params['clubId'];
+    super.isLoading().subscribe(res => this.loading = res);
   }
 
-  ngOnInit() { }
+  private fetch() {
+    let search = new URLSearchParams();
+    search.append('school_id', this.session.school.id.toString());
+
+    super
+      .fetchData(this.clubsService.getClubById(this.clubId, search))
+      .then(club => {
+        this.hasMembership = club.data.has_membership;
+
+        this.store.dispatch({
+          type: HEADER_UPDATE,
+          payload: this.buildHeader(club.data.name)
+        });
+      });
+  }
+
+  buildHeader(name) {
+    let schoolPrivileges = this.session.user.school_level_privileges[this.session.school.id];
+    let accountPrivilege = this.session.user.account_level_privileges[this.clubId];
+    let menu = {
+      heading: name,
+      subheading: null,
+      em: null,
+      children: []
+    };
+
+    let links = ['Info'];
+
+    if (this.hasMembership) {
+      links = ['Members', ...links];
+    }
+
+    if (schoolPrivileges) {
+      if (schoolPrivileges[CP_PRIVILEGES_MAP.events].r) {
+        links = ['Events', ...links];
+      }
+
+      if (schoolPrivileges[CP_PRIVILEGES_MAP.moderation].r) {
+        links = ['Wall', ...links];
+      }
+    }
+
+    if (accountPrivilege) {
+      if (links.indexOf('Events') === -1 &&
+        accountPrivilege[CP_PRIVILEGES_MAP.events] &&
+        accountPrivilege[CP_PRIVILEGES_MAP.events].r) {
+        links = ['Events', ...links];
+      }
+
+      if (links.indexOf('Wall') === -1 &&
+        accountPrivilege[CP_PRIVILEGES_MAP.moderation]
+        && accountPrivilege[CP_PRIVILEGES_MAP.moderation].r) {
+        links = ['Wall', ...links];
+      }
+    }
+
+    links.forEach(link => {
+      menu.children.push({
+        label: link,
+        url: `/manage/clubs/${this.clubId}/${link.toLocaleLowerCase()}`
+      });
+    });
+
+    return menu;
+  }
+
+  ngOnInit() {
+    this.fetch();
+  }
 }
 
