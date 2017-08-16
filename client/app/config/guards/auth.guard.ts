c449e8e8bce199dc92830057915d9f2686fe8b1e
zacum <mfilgyhty@hotmail.com>
2017-08-16 10:03:21 -0400

    add can activate child

--- client/app/config/guards/auth.guard.ts
+++ client/app/config/guards/auth.guard.ts
@@ -1,17 +1,18 @@
 /**
  * Guard to check if user is authenticated
  */
-import { Router } from '@angular/router';
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot } from '@angular/router';
 import { URLSearchParams } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { CPSession } from '../../session';
 import { appStorage } from '../../shared/utils';
+// import { CP_PRIVILEGES } from './../../shared/utils/privileges';
 import { AdminService, SchoolService } from '../../shared/services';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(
     private router: Router,
     private session: CPSession,
@@ -19,33 +20,74 @@ export class AuthGuard implements CanActivate {
     private schoolService: SchoolService
   ) { }
 
+  preLoadUser() {
+    const school$ = this.schoolService.getSchools();
+
+    return school$
+      .switchMap(schools => {
+        let search = new URLSearchParams();
+        let storedSchool = JSON.parse(appStorage.get(appStorage.keys.DEFAULT_SCHOOL));
+
+        this.session.schools = schools;
+
+        this.session.school = storedSchool || schools[0];
+
+        search.append('school_id', this.session.school.id.toString());
+
+        return this.adminService.getAdmins(1, 1, search);
+      })
+      .toPromise()
+  }
+
+  checkGoToRedirectPrivileges(_) {
+    // const privilege = CP_PRIVILEGES[state.url.split('/')[state.url.split('/').length -1]];
+    return new Promise((resolve, reject) => {
+      if (this.session.privileges['readService']) {
+        resolve(true);
+        return;
+      }
+
+      this.router.navigate(['welcome']);
+      reject(false);
+    });
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot) {
+    const MANAGE_ROUTES = ['events', 'feeds', 'clubs', 'services', 'lists', 'links'];
+
+    if (childRoute.url.length) {
+      const routeName = childRoute.url[0].path;
+
+      // if (MANAGE_ROUTES.includes(routeName)) {
+
+      // }
+      return true;
+    }
+    return true;
+  }
+
   canActivate(__, state) {
+    // are we logged in?
     if (appStorage.get(appStorage.keys.SESSION)) {
-      const school$ = this.schoolService.getSchools();
 
+      // did we create the session object?
       if (!this.session.school || !this.session.user) {
-        return school$
-          .switchMap(schools => {
-            let search = new URLSearchParams();
-            let storedSchool = JSON.parse(appStorage.get(appStorage.keys.DEFAULT_SCHOOL));
 
-            this.session.schools = schools;
+        return this.preLoadUser()
 
-            this.session.school = storedSchool || schools[0];
-
-            search.append('school_id', this.session.school.id.toString());
-
-            return this.adminService.getAdmins(1, 1, search);
-          })
-          .toPromise()
           .then(user => {
             this.session.user = user[0];
             return this.session.updateSessionPrivileges();
           })
+
+          .then(_ => this.checkGoToRedirectPrivileges(state))
+
           .then(_ => true)
+
           .catch(_ => false);
       }
-      return true;
+
+      return true
     }
 
     this.router.navigate(
