5af67c6c02c97c41edf5c5dc7f6cb21c6089a4a6
zacum <mfilgyhty@hotmail.com>
2017-08-18 15:45:04 -0400

    list page

--- client/app/containers/controlpanel/assess/students/list/engagement-students-list.component.ts
+++ client/app/containers/controlpanel/assess/students/list/engagement-students-list.component.ts
@@ -1,12 +1,82 @@
+import { URLSearchParams } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
+
+import { StudentsService } from './../students.service';
+import { CPSession } from './../../../../../session/index';
+import { FORMAT } from './../../../../../shared/pipes/date.pipe';
+import { BaseComponent } from './../../../../../base/base.component';
+
+interface IState {
+  search_str: string,
+  list_id: number
+}
 
 @Component({
   selector: 'cp-engagement-students-list',
   templateUrl: './engagement-students-list.component.html',
   styleUrls: ['./engagement-students-list.component.scss']
 })
-export class EngagementStudentsListComponent implements OnInit {
-  constructor() { }
+export class EngagementStudentsListComponent extends BaseComponent implements OnInit {
+  loading;
+  students = [];
+  state: IState = {
+    search_str: null,
+    list_id: null,
+  }
 
-  ngOnInit() { }
+  dateFormat = FORMAT.DATETIME
+
+  constructor(
+    private session: CPSession,
+    private service: StudentsService
+  ) {
+    super();
+    super.isLoading().subscribe(loading => this.loading = loading);
+  }
+
+  fetch() {
+    const search = new URLSearchParams();
+    const list_id =  this.state.list_id ? this.state.list_id.toString() : null;
+
+    search.append('school_id', this.session.school.id.toString());
+    search.append('search_str', this.state.search_str);
+    search.append('list_id', list_id);
+
+    const stream$ = this.service.getStudentsByList(search, this.startRange, this.endRange);
+    super
+      .fetchData(stream$)
+      .then(res => this.students = res.data)
+      .catch(err => console.log(err));
+
+  }
+
+  onPaginationNext(): void {
+    super.goToNext();
+    this.fetch();
+  }
+
+  onPaginationPrevious(): void {
+    super.goToPrevious();
+    this.fetch();
+  }
+
+  messageStudent(student) {
+    console.log('messaging ', student);
+  }
+
+  onFilter(filterBy) {
+    this.state = Object.assign(
+      {},
+      this.state,
+      {
+        search_str: filterBy.search_str,
+        list_id: filterBy.list_id,
+      }
+    )
+    this.fetch();
+  }
+
+  ngOnInit() {
+    this.fetch();
+  }
 }
--- client/app/containers/controlpanel/assess/students/students.service.ts
+++ client/app/containers/controlpanel/assess/students/students.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Observable';
 import { Http, URLSearchParams } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
@@ -18,5 +19,33 @@ export class StudentsService extends BaseService {
     const url = `${common}/${startRange};${endRange}`;
 
     return super.get(url, { search }).map(res => res.json());
+  }
+
+  getStudentsByList(search: URLSearchParams, startRange: number, endRange: number) {
+    console.log(search, startRange, endRange);
+
+    return Observable.of([
+      {
+        id: 16776,
+        first_name: 'Andres',
+        last_name: 'Roget',
+        last_seven_days: {
+          events: 8,
+          services: 12
+        },
+        last_engagement: 1503083192
+      },
+      {
+        id: 16776,
+        first_name: 'Anas',
+        last_name: 'Al-Khatib',
+        last_seven_days: {
+          events: 3,
+          services: 0
+        },
+        last_engagement: 1503083268
+      }
+    ])
+    .delay(1200);
   }
 }
