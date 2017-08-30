87b442ec5e5587ec5bfc1ded8ff9bda3615d121b
zacum <mfilgyhty@hotmail.com>
2017-08-30 16:23:04 -0400

    sentry

--- client/app/config/providers/index.ts
+++ client/app/config/providers/index.ts
@@ -1,6 +1,13 @@
 import { AuthGuard } from '../guards';
+import { RavenErrorHandler } from './raven.handler';
 import { ErrorService } from '../../shared/services';
+import { ErrorHandler } from '@angular/core';
 
 import { CPSession } from '../../session';
 
-export const APP_PROVIDERS = [ CPSession, AuthGuard, ErrorService ];
+export const APP_PROVIDERS = [
+  CPSession,
+  AuthGuard,
+  ErrorService,
+  { provide: ErrorHandler, useClass: RavenErrorHandler }
+];
