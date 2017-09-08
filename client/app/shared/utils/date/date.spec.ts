7df026d35c26b60b48684c951f9e33ceff7a3538
zacum <mfilgyhty@hotmail.com>
2017-09-08 12:01:38 -0400

    date object test

---
+++ client/app/shared/utils/date/date.spec.ts
@@ -0,0 +1,16 @@
+import { CPDate } from './date';
+
+import * as moment from 'moment';
+
+const mockDateObj = moment('December 22nd 2016, 2:00 pm', 'MMMM Do YYYY, h:mm a');
+const mockEpochDate = 1482433200;
+
+describe('CPDate', () => {
+  it('Should convert a valid Date object to epoch time', () => {
+    expect(CPDate.toEpoch(mockDateObj)).toBe(mockEpochDate);
+  })
+
+  it('Should convert an epoch timestamp into a valid Date Object', () => {
+    expect(CPDate.fromEpoch(mockEpochDate)).toEqual(mockDateObj.toDate());
+  })
+})
