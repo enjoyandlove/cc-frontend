3d46bdbb6c7e6de5ef7aa007a5c4ea9b93093379
zacum <mfilgyhty@hotmail.com>
2017-09-08 14:53:15 -0400

    rename base64 to encrypt

--- client/app/shared/utils/index.ts
+++ client/app/shared/utils/index.ts
@@ -2,5 +2,5 @@ export * from './map';
 export * from './date';
 export * from './image';
 export * from './object';
-export * from './base64';
 export * from './storage';
+export * from './encrypt';
