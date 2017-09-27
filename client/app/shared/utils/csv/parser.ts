2a9325cc892412960e704a099126f2c55711f89d
zacum <mfilgyhty@hotmail.com>
2017-09-27 15:21:26 -0400

    update to Blob

--- client/app/shared/utils/csv/parser.ts
+++ client/app/shared/utils/csv/parser.ts
@@ -2,25 +2,21 @@ import { unparse } from 'papaparse';
 import { saveAs } from 'file-saver';
 
 export const createSpreadSheet = (data: any[], fields: Array<string>, filename = 'download') => {
-  console.log('calling createSpreadSheet with ', data);
 
   const parseData = unparse({
     fields,
     data: [...data]
   })
 
-  console.log('parseData ', parseData);
+  // const file = new File(
+  //   [parseData],
+  //   `${filename}.csv`,
+  //   { type: 'text/csv;charset=utf-8' }
+  // );
 
-  const file = new File(
-    [parseData],
-    `${filename}.csv`,
-    { type: 'text/csv;charset=utf-8' }
-  );
-
-  console.log('file ', file);
-  saveAs(file);
-
-  console.log('saveAs');
+  const file = new Blob([parseData], { type: 'text/csv;charset=utf-8' });
+  console.log('Yo New', file);
+  saveAs(file, `${filename}.csv`);
 };
 
 
