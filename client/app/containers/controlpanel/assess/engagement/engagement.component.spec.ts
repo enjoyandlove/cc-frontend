477b0c8396ede2275c00366b59bdbd3a341f7be6
zacum <mfilgyhty@hotmail.com>
2017-10-17 10:28:17 -0400

    more tests

---
+++ client/app/containers/controlpanel/assess/engagement/engagement.component.spec.ts
@@ -0,0 +1,127 @@
+import { Observable } from 'rxjs/Observable';
+import { Store, StoreModule } from '@ngrx/store';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+import { CPSession } from './../../../../session/index';
+import { EngagementService } from './engagement.service';
+import { mockUser } from './../../../../session/mock/user';
+import { EngagementComponent } from './engagement.component';
+import { mockSchool } from './../../../../session/mock/school';
+import { snackBarReducer, headerReducer } from '../../../../reducers';
+
+const mockFilterState = {
+  'engagement': {
+    'route_id': 'all',
+    'label': 'All Engagements',
+    'data': {
+      'type': null,
+      'value': 0,
+      'queryParam': 'scope'
+    }
+  },
+  'for': {
+    'route_id': 'all_students',
+    'label': 'All Students',
+    'listId': null
+  },
+  'range': {
+    'route_id': 'last_week',
+    'label': 'Last 7 Days',
+    'payload': {
+      'metric': 'daily',
+      'range': {
+        'end': 1508180917,
+        'start': 1507608000
+      }
+    }
+  }
+}
+
+class MockEngagementService {
+  getChartData() {
+    return Observable.of('hello');
+  }
+}
+
+class MockSession {
+  g = new Map();
+};
+
+class MockRouter {
+  navigate(url: string) { return url; }
+};
+
+describe('EngagementComponent', () => {
+  let store: Store<any>;
+  // let session: CPSession;
+  let component: EngagementComponent;
+  let fixture: ComponentFixture<EngagementComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        // EngagementModule,
+        StoreModule.provideStore({
+          HEADER: headerReducer,
+          SNACKBAR: snackBarReducer
+        })
+      ],
+      declarations: [ EngagementComponent ],
+      providers: [
+        { provide: Router,  useClass: MockRouter },
+        { provide: CPSession, useClass: MockSession },
+        { provide: EngagementService, useClass: MockEngagementService },
+        {
+          provide: ActivatedRoute, useValue: {
+            'snapshot': {
+              'params': Observable.of({ studentId: 1 })
+            }
+          }
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    .overrideComponent(EngagementComponent, {
+      set: {
+        template: '<div>No need of child components</div>'
+      }
+    })
+
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+    fixture = TestBed.createComponent(EngagementComponent);
+    component = fixture.componentInstance;
+
+    component.session.g.set('user', mockUser);
+    component.session.g.set('school', mockSchool);
+
+    fixture.detectChanges();
+  });
+
+  it('onDoCompose', () => {
+    const expected = { to: 'me', message: 'hello world' };
+
+    expect(component.messageData).not.toBeDefined();
+    expect(component.isComposeModal).toBeFalsy();
+
+    component.onDoCompose(expected);
+
+    expect(component.isComposeModal).toBeTruthy();
+    expect(component.messageData).toEqual(expected);
+  })
+
+  it('onComposeTeardown', () => {
+    component.onComposeTeardown();
+
+    expect(component.isComposeModal).toBeFalsy();
+    expect(component.messageData).toBeNull();
+  })
+
+  it('buildSearchHeaders', () => {
+    component.onDoFilter(mockFilterState);
+    fixture.detectChanges();
+    console.log(component.buildSearchHeaders().paramsMap.toJSON());
+  })
+})
