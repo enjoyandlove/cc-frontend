8ab93dc6e48cc271394361ec83ebe36cb6c2d6a2
zacum <mfilgyhty@hotmail.com>
2017-10-31 10:25:54 -0400

    update placeholder to Select Hosts

--- client/app/shared/services/store.service.ts
+++ client/app/shared/services/store.service.ts
@@ -104,7 +104,7 @@ export class StoreService extends BaseService {
       });
   }
 
-  getStores(search: URLSearchParams, placeHolder = 'All Hosts') {
+  getStores(search: URLSearchParams, placeHolder = 'Select Hosts') {
     const clubs$ = this.getClubs(search);
     const services$ = this.getServices(search);
 
@@ -133,88 +133,6 @@ export class StoreService extends BaseService {
         ];
       })
   }
-
-  //   getStores(search: URLSearchParams) {
-  //     const url = `${API.BASE_URL}/${API.VERSION.V1}/${API.ENDPOINTS.STORE}/1;1000`;
-
-  // if (!search.get('event_host_only')) {
-  //   search.append('event_host_only', '1');
-  // }
-
-  //     return super
-  //       .get(url, { search })
-  //       .map(res => {
-  //         return res.json();
-  //       })
-  //       .startWith([{ 'label': 'All Hosts' }])
-  //       .map(res => {
-  //         const CLUBS_CATEGORY = 0;
-  //         const SERVICES_CATEGORY = 19;
-
-  // let stores = [
-  //   {
-  //     'label': 'Select Host',
-  //     'value': null,
-  //     'heading': true,
-  //   }
-  // ];
-
-  // let clubs = [
-  //   {
-  //     'label': 'Clubs',
-  //     'value': null,
-  //     'heading': true,
-  //   }
-  // ];
-
-  // let services = [
-  //   {
-  //     'label': 'Services',
-  //     'value': null,
-  //     'heading': true,
-  //   }
-  // ];
-
-  //   res.forEach(store => {
-  //     if (store.category_id === CLUBS_CATEGORY) {
-  //       clubs.push({
-  //         'label': store.name,
-  //         'value': store.id,
-  //         'heading': false,
-  //       });
-  //     }
-  //     if (store.category_id === SERVICES_CATEGORY) {
-  //       services.push({
-  //         'label': store.name,
-  //         'value': store.id,
-  //         'heading': false,
-  //       });
-  //     }
-  //   });
-
-  //   if (clubs.length > 1) {
-  //     stores.push(...clubs);
-  //   }
-
-  //   if (services.length > 1) {
-  //     stores.push(...services);
-  //   }
-
-  //   if (clubs.length === 1 && services.length === 1) {
-  //     stores[0] = Object.assign(
-  //       {},
-  //       stores[0],
-  //       {
-  //         disabled: true,
-  //         label: 'No Hosts Available',
-  //         tooltipText: 'No Services or Clubs found, check your account privileges'
-  //       }
-  //     );
-  //   }
-
-  //   return stores;
-  // });
-  //   }
 
   getStoreById(storeId: number) {
     const url = `${API.BASE_URL}/${API.VERSION.V1}/${API.ENDPOINTS.STORE}/${storeId}`;
