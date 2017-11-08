73ade59467f8584eee239ffd455414b332da1976
zacum <mfilgyhty@hotmail.com>
2017-11-08 10:01:31 -0500

    update pipe to take contenxt

--- client/app/shared/pipes/i18n/i18n.pipe.ts
+++ client/app/shared/pipes/i18n/i18n.pipe.ts
@@ -6,5 +6,11 @@ const cpI18n = new CPI18nService();
 
 @Pipe({name: 'cpI18n'})
 export class CPI18nPipe implements PipeTransform {
-  transform(key: string): any { return cpI18n.translate(key); }
+  transform(key: string, context?: any): any {
+    const translatedString = cpI18n.translate(key);
+
+    return context ?
+           translatedString.replace('%s', context) :
+           translatedString;
+  }
 }
