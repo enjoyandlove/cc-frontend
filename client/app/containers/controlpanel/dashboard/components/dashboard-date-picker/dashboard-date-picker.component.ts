86053f8c268b881e32569288db31a2f381a3a32b
zacum <mfilgyhty@hotmail.com>
2017-12-04 11:01:57 -0500

    update styles and events on date picker

--- client/app/containers/controlpanel/dashboard/components/dashboard-date-picker/dashboard-date-picker.component.ts
+++ client/app/containers/controlpanel/dashboard/components/dashboard-date-picker/dashboard-date-picker.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { DashboardUtilsService } from './../../dashboard.utils.service';
+
+interface IDateChange {
+  end: number,
+  start: number,
+  label: string,
+}
 
 @Component({
   selector: 'cp-dashboard-date-picker',
@@ -8,14 +14,36 @@ import { DashboardUtilsService } from './../../dashboard.utils.service';
   styleUrls: ['./dashboard-date-picker.component.scss']
 })
 export class DashboardDatePickerComponent implements OnInit {
+  @Output() dateChange: EventEmitter<IDateChange> = new EventEmitter();
+
+  selected = null;
+  customDates = [];
+
   constructor(
     private helper: DashboardUtilsService
   ) { }
 
+  handleCustomDate(date) {
+    this.setLabel(date);
+    this.triggerChange();
+  }
+
+  triggerChange() {
+    this.dateChange.emit(this.selected);
+  }
+
+  setLabel(date) {
+    this.selected = date;
+  }
+
   ngOnInit() {
-    console.log(this.helper.last30Days());
-    console.log(this.helper.last90Days());
-    console.log(this.helper.lastYear());
-    console.log(this.helper.allTime());
+    this.customDates = [
+      this.helper.last30Days(),
+      this.helper.last90Days(),
+      this.helper.lastYear(),
+      this.helper.allTime()
+    ]
+
+    this.setLabel(this.customDates[0]);
   }
 }
