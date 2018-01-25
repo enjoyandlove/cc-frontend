146a123bbd8f50fed89028037f9fda7f73097199
zacum <mfilgyhty@hotmail.com>
2018-01-25 16:42:21 -0500

    track click on go to old cp

--- client/app/shared/components/cp-top-banner/cp-top-banner.component.ts
+++ client/app/shared/components/cp-top-banner/cp-top-banner.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { isCanada } from './../../../config/env/index';
+import { CP_TRACK_TO } from '../../directives/index';
 
 @Component({
   selector: 'cp-top-banner',
@@ -8,11 +9,18 @@ import { isCanada } from './../../../config/env/index';
   styleUrls: ['./cp-top-banner.component.scss'],
 })
 export class CPTopBanerComponent implements OnInit {
+  eventData;
   oldCPUrl: string;
 
   constructor() {}
 
   ngOnInit() {
+    this.eventData = {
+      type: CP_TRACK_TO.GA,
+      eventAction: 'Click Event',
+      eventCategory: 'Go Back To CP',
+    };
+
     this.oldCPUrl = isCanada
       ? 'https://ca.oohlalamobile.com/login?no_redirect=true'
       : 'https://oohlalamobile.com/login?no_redirect=true';
