89af3f08b073f3b81b06199e7de7cd6038086e4a
zacum <mfilgyhty@hotmail.com>
2018-02-09 14:07:28 -0500

    update french months to take capital names

--- client/app/shared/pipes/date/date.pipe.ts
+++ client/app/shared/pipes/date/date.pipe.ts
@@ -11,9 +11,27 @@ export const FORMAT = {
   TIME: 'h:mm A',
 };
 
+moment.updateLocale('fr-CA', {
+  months: [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre',
+  ],
+});
+
 @Pipe({ name: 'cpDatePipe' })
 export class CPDatePipe implements PipeTransform {
   transform(date: number, format: string) {
+    console.log(CPI18nService.getLocale());
     moment.locale(CPI18nService.getLocale());
 
     return moment.unix(date).format(format);
