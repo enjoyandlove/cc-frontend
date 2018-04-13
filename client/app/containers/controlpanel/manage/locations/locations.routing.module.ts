dfb31bff039dbee0aedee9fdfe739cd048327959
zacum <mfilgyhty@hotmail.com>
2018-04-13 16:35:27 +0500

    canActivateChild guard move to child level, new canActivate guard added with privileges.

--- client/app/containers/controlpanel/manage/locations/locations.routing.module.ts
+++ client/app/containers/controlpanel/manage/locations/locations.routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LocationsListComponent } from './list';
+import { PrivilegesGuard } from '../../../../config/guards';
 
 const appRoutes: Routes = [
   {
     path: '',
+    canActivate: [PrivilegesGuard],
     component: LocationsListComponent,
     data: { zendesk: 'locations' },
   },
