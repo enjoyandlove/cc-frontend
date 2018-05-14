47ee2603f3b989657a4d761acf0789147dd19220
zacum <mfilgyhty@hotmail.com>
2018-05-14 15:32:17 +0000

    Feature/web 1679 hide email of deleted users

--- client/app/containers/controlpanel/manage/feeds/list/components/feed-header/feed-header.component.ts
+++ client/app/containers/controlpanel/manage/feeds/list/components/feed-header/feed-header.component.ts
@@ -1,20 +1,27 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
-import { FORMAT } from '../../../../../../../shared/pipes/date';
+import { UserStatus } from './../../feeds.status';
 import { CPDate } from '../../../../../../../shared/utils';
+import { FORMAT } from '../../../../../../../shared/pipes/date';
+import { CPI18nService } from '../../../../../../../shared/services';
 
 @Component({
   selector: 'cp-feed-header',
   templateUrl: './feed-header.component.html',
   styleUrls: ['./feed-header.component.scss']
 })
-export class FeedHeaderComponent {
+export class FeedHeaderComponent implements OnInit {
   @Input() feed: any;
   @Input() isComment: boolean;
   @Output() filterByCategory: EventEmitter<any> = new EventEmitter();
   state: any;
   CPDate = CPDate;
+  emailTextContent;
   FORMAT = FORMAT.DATETIME;
+  activeUser = UserStatus.active;
+  deletedUser = UserStatus.deleted;
+
+  constructor(public cpI18n: CPI18nService) {}
 
   loadCategory(item) {
     this.state = Object.assign({}, this.state, {
@@ -24,5 +31,12 @@ export class FeedHeaderComponent {
       action: item.post_type
     });
     this.filterByCategory.emit(this.state);
+  }
+
+  ngOnInit() {
+    this.emailTextContent =
+      this.feed.user_status === UserStatus.active
+        ? `(${this.feed.email})`
+        : `(${this.cpI18n.translate('feeds_card_header_deleted_user')})`;
   }
 }
