e781229042e019f941c7fe26c61e045449d62416
zacum <mfilgyhty@hotmail.com>
2018-05-22 17:48:11 +0500

    deals create, edit, delete unit tests added

---
+++ client/app/containers/controlpanel/manage/deals/delete/deals-delete.component.spec.ts
@@ -0,0 +1,81 @@
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { DealsModule } from '../deals.module';
+import { DealsService } from '../deals.service';
+import { CPSession } from '../../../../../session';
+import { DealsDeleteComponent } from './deals-delete.component';
+import { CPI18nService } from '../../../../../shared/services';
+import { mockSchool } from '../../../../../session/mock/school';
+
+class MockDealsService {
+  dummy;
+
+  deleteDeal(id: number, search: any) {
+    this.dummy = [id, search];
+
+    return Observable.of({});
+  }
+}
+
+describe('DealsDeleteComponent', () => {
+  let spy;
+  let search;
+  let component: DealsDeleteComponent;
+  let fixture: ComponentFixture<DealsDeleteComponent>;
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [DealsModule],
+        providers: [
+          CPSession,
+          CPI18nService,
+          { provide: DealsService, useClass: MockDealsService },
+        ]
+      })
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(DealsDeleteComponent);
+          component = fixture.componentInstance;
+
+          search = new URLSearchParams();
+          component.deal = {
+            id: 1,
+            title: 'Hello World!',
+            start: 0,
+            expiration: 0,
+            image_url: 'dummy.jpeg',
+            description: 'This is description'
+          };
+
+          component.session.g.set('school', mockSchool);
+          search.append('school_id', component.session.g.get('school').id.toString());
+        });
+    })
+  );
+
+  it('buttonData should have "Delete" label & "Danger class"', () => {
+    component.ngOnInit();
+    expect(component.buttonData.text).toEqual('Delete');
+    expect(component.buttonData.class).toEqual('danger');
+  });
+
+  it('should delete deal', () => {
+    spyOn(component.deleted, 'emit');
+    spyOn(component.resetDeleteModal, 'emit');
+    spy = spyOn(component.service, 'deleteDeal').and.returnValue(Observable.of({}));
+
+    component.onDelete();
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(component.deal.id, search);
+
+    expect(component.deleted.emit).toHaveBeenCalledTimes(1);
+    expect(component.deleted.emit).toHaveBeenCalledWith(component.deal.id);
+
+    expect(component.resetDeleteModal.emit).toHaveBeenCalled();
+    expect(component.resetDeleteModal.emit).toHaveBeenCalledTimes(1);
+  });
+
+});
---
+++ client/app/containers/controlpanel/manage/deals/edit/deals-edit.component.spec.ts
@@ -0,0 +1,140 @@
+import { async, TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { StoreModule } from '@ngrx/store';
+
+import { DealsModule } from '../deals.module';
+import { DealsService } from '../deals.service';
+import { CPSession } from '../../../../../session';
+import { StoreService } from '../stores/store.service';
+import { CPI18nService } from '../../../../../shared/services';
+import { mockSchool } from '../../../../../session/mock/school';
+import { DealsEditComponent } from './deals-edit.component';
+import { headerReducer, snackBarReducer } from '../../../../../reducers';
+
+class MockDealsService {
+  dummy;
+
+  editDeal(body: any, search: any) {
+    this.dummy = [body, search];
+
+    return Observable.of({});
+  }
+
+  getDealById(id: number, search: any) {
+    this.dummy = [id, search];
+
+    return Observable.of({});
+  }
+}
+
+class MockStoreService {
+  dummy;
+
+  createStore(body: any, search: any) {
+    this.dummy = [body, search];
+
+    return Observable.of({});
+  }
+}
+
+fdescribe('DealsEditComponent', () => {
+  let spyDeal;
+  let search;
+  let spyStore;
+  let spyFetchDeals;
+  let component: DealsEditComponent;
+  let fixture: ComponentFixture<DealsEditComponent>;
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          HttpModule,
+          DealsModule,
+          RouterTestingModule,
+          StoreModule.forRoot({
+            HEADER: headerReducer,
+            SNACKBAR: snackBarReducer
+          })
+        ],
+        providers: [
+          CPSession,
+          CPI18nService,
+          { provide: StoreService, useClass: MockStoreService },
+          { provide: DealsService, useClass: MockDealsService },
+        ]
+      })
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(DealsEditComponent);
+          component = fixture.componentInstance;
+          search = new URLSearchParams();
+
+          component.dealId = 1;
+          component.session.g.set('school', mockSchool);
+          search.append('school_id', component.session.g.get('school').id);
+
+          spyOn(component.router, 'navigate');
+
+          spyDeal = spyOn(component.service, 'editDeal')
+            .and.returnValue(Observable.of({}));
+
+          spyFetchDeals = spyOn(component.service, 'getDealById')
+            .and.returnValue(Observable.of({}));
+
+          spyStore = spyOn(component.storeService, 'createStore')
+            .and.returnValue(Observable.of({}));
+        });
+    })
+  );
+
+  it('fetch deals', () => {
+    component.ngOnInit();
+    expect(spyFetchDeals).toHaveBeenCalledTimes(1);
+    expect(spyFetchDeals).toHaveBeenCalledWith(component.dealId, search);
+  });
+
+  it('editDeal', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+
+    component.isNewStore = false;
+    component.data = {
+      deal: component.form.value,
+      store: null,
+    };
+
+    component.onSubmit();
+
+    expect(spyDeal).toHaveBeenCalled();
+    expect(spyDeal).toHaveBeenCalledTimes(1);
+
+    expect(spyFetchDeals).toHaveBeenCalledTimes(1);
+    expect(spyFetchDeals).toHaveBeenCalledWith(component.dealId, search);
+  }));
+
+  it('editDealWithNewStore', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+
+    component.isNewStore = true;
+    component.data = {
+      deal: component.form.value,
+      store: component.storeForm.value,
+    };
+
+    component.onSubmit();
+
+    expect(spyDeal).toHaveBeenCalled();
+    expect(spyDeal).toHaveBeenCalledTimes(1);
+
+    expect(spyStore).toHaveBeenCalled();
+    expect(spyStore).toHaveBeenCalledTimes(1);
+
+    expect(spyFetchDeals).toHaveBeenCalledTimes(1);
+    expect(spyFetchDeals).toHaveBeenCalledWith(component.dealId, search);
+  }));
+
+});
