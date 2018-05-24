52952afa8e0a4c0040d025393c9b42c7ebce4bd5
zacum <mfilgyhty@hotmail.com>
2018-05-24 09:29:42 -0400

    updates on sorting

--- client/app/containers/controlpanel/customise/personas/list/list.component.ts
+++ client/app/containers/controlpanel/customise/personas/list/list.component.ts
@@ -9,6 +9,7 @@ import { BaseComponent } from '../../../../../base';
 import { PersonasService } from './../personas.service';
 import { CPI18nService } from './../../../../../shared/services/i18n.service';
 import { HEADER_UPDATE, IHeader } from './../../../../../reducers/header.reducer';
+import { ISnackbar, SNACKBAR_SHOW } from './../../../../../reducers/snackbar.reducer';
 
 @Component({
   selector: 'cp-personas-list',
@@ -18,6 +19,7 @@ import { HEADER_UPDATE, IHeader } from './../../../../../reducers/header.reducer
 export class PersonasListComponent extends BaseComponent implements OnInit {
   loading;
   state = {
+    updating: false,
     platform: null,
     search_str: null,
     personas: <Array<IPersona>>[]
@@ -26,8 +28,8 @@ export class PersonasListComponent extends BaseComponent implements OnInit {
   constructor(
     public session: CPSession,
     public cpI18n: CPI18nService,
-    public store: Store<IHeader>,
-    public service: PersonasService
+    public service: PersonasService,
+    public store: Store<IHeader | ISnackbar>
   ) {
     super();
     super.isLoading().subscribe((loading) => (this.loading = loading));
@@ -61,8 +63,39 @@ export class PersonasListComponent extends BaseComponent implements OnInit {
     this.fetch();
   }
 
+  updatePersona(persona: IPersona): Promise<any> {
+    const personaId = persona.id;
+    const schoolId = this.session.g.get('school').id;
+
+    const search = new URLSearchParams();
+    search.append('school_id', schoolId);
+
+    delete persona['id'];
+    persona['school_id'] = schoolId;
+
+    // a rank of zero is not allowed
+    console.log(1, persona);
+    persona = { ...persona, rank: persona.rank + 1 };
+    console.log(2, persona);
+
+    return this.service.updatePersona(personaId, search, persona).toPromise();
+  }
+
+  handleUpdateError() {
+    this.state = { ...this.state, updating: false };
+
+    this.store.dispatch({
+      type: SNACKBAR_SHOW,
+      payload: {
+        autoClose: true,
+        class: 'danger',
+        body: this.cpI18n.translate('something_went_wrong')
+      }
+    });
+  }
+
   movePersonaToIndex(persona, currentIndex, newIndex) {
-    // avoid mutating the object
+    // avoid mutating the original personas
     const clonedPersonas = [...this.state.personas];
 
     // remove persona from current index
@@ -78,20 +111,70 @@ export class PersonasListComponent extends BaseComponent implements OnInit {
     const firstItemInList = currentIndex === 0;
     const newIndex = firstItemInList ? this.state.personas.length - 1 : currentIndex - 1;
 
-    this.state = {
-      ...this.state,
-      personas: this.movePersonaToIndex(persona, currentIndex, newIndex)
+    const movingPersona = {
+      ...this.state.personas[currentIndex],
+      rank: newIndex
     };
+
+    const movedPersona = {
+      ...this.state.personas[currentIndex - 1],
+      rank: newIndex - 1
+    };
+
+    this.state = { ...this.state, updating: true };
+
+    const updatePersonas = Promise.all([
+      this.updatePersona(movingPersona),
+      this.updatePersona(movedPersona)
+    ]);
+
+    updatePersonas
+      .then(() => {
+        this.state = { ...this.state, updating: false };
+
+        const personas = this.movePersonaToIndex(persona, currentIndex, newIndex);
+
+        this.state = {
+          ...this.state,
+          personas
+        };
+      })
+      .catch(() => this.handleUpdateError());
   }
 
   onRankDown(persona: IPersona, currentIndex: number) {
     const lastItemInList = currentIndex === this.state.personas.length - 1;
     const newIndex = lastItemInList ? 0 : currentIndex + 1;
 
-    this.state = {
-      ...this.state,
-      personas: this.movePersonaToIndex(persona, currentIndex, newIndex)
+    const movingPersona = {
+      ...this.state.personas[currentIndex],
+      rank: newIndex
     };
+
+    const movedPersona = {
+      ...this.state.personas[lastItemInList ? 0 : currentIndex + 1],
+      rank: newIndex + 1
+    };
+
+    this.state = { ...this.state, updating: true };
+
+    const updatePersonas = Promise.all([
+      this.updatePersona(movingPersona),
+      this.updatePersona(movedPersona)
+    ]);
+
+    updatePersonas
+      .then(() => {
+        this.state = { ...this.state, updating: false };
+
+        const personas = this.movePersonaToIndex(persona, currentIndex, newIndex);
+
+        this.state = {
+          ...this.state,
+          personas
+        };
+      })
+      .catch(() => this.handleUpdateError());
   }
 
   fetch() {
