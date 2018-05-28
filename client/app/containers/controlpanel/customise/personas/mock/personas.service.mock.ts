3897637b7d9c488ced76ef86f09176d270995912
zacum <mfilgyhty@hotmail.com>
2018-05-28 15:56:39 -0400

    edit

--- client/app/containers/controlpanel/customise/personas/mock/personas.service.mock.ts
+++ client/app/containers/controlpanel/customise/personas/mock/personas.service.mock.ts
@@ -1,40 +1,48 @@
 import { Observable } from 'rxjs/Observable';
 
+export const mockPersonas = [
+  {
+    login_requirement: 1,
+    cre_enabled: false,
+    rank: 1,
+    platform: 0,
+    localized_name_map: {
+      fr: "Student's Tile",
+      en: "Student's Tile"
+    },
+    pretour_enabled: false,
+    id: 1
+  },
+  {
+    login_requirement: -1,
+    cre_enabled: false,
+    rank: 2,
+    platform: 1,
+    localized_name_map: {
+      fr: 'Web Persona Bonjour',
+      en: 'Web Persona'
+    },
+    pretour_enabled: false,
+    id: 89
+  }
+];
+
 export class MockPersonasService {
   dummy;
 
   getPersonas() {
-    return Observable.of([
-      {
-        login_requirement: 1,
-        cre_enabled: false,
-        rank: 1,
-        platform: 0,
-        localized_name_map: {
-          fr: "Student's Tile",
-          en: "Student's Tile"
-        },
-        pretour_enabled: false,
-        id: 1
-      },
-      {
-        login_requirement: -1,
-        cre_enabled: false,
-        rank: 2,
-        platform: 1,
-        localized_name_map: {
-          fr: 'Web Persona Bonjour',
-          en: 'Web Persona'
-        },
-        pretour_enabled: false,
-        id: 89
-      }
-    ]);
+    return Observable.of(mockPersonas);
   }
 
   updatePersona(personaId, search, persona) {
     this.dummy = { personaId, search };
 
     return Observable.of(persona);
+  }
+
+  getPersonaById(personaId, search) {
+    this.dummy = { personaId, search };
+
+    return Observable.of(mockPersonas.filter((p) => p.id === personaId)[0]);
   }
 }
