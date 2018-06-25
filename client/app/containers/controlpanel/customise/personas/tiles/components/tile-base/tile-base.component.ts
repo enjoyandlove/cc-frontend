303936c5b5a3021ee8246ce5e609d65729f028b6
zacum <mfilgyhty@hotmail.com>
2018-06-25 20:35:10 +0000

    Feature/web 1860 feature tiles

--- client/app/containers/controlpanel/customise/personas/tiles/components/tile-base/tile-base.component.ts
+++ client/app/containers/controlpanel/customise/personas/tiles/components/tile-base/tile-base.component.ts
@@ -11,7 +11,6 @@ import { Component, OnInit } from '@angular/core';
       width: 100%;
       height: 158px;
       position: relative;
-      margin-bottom: 30px;
       box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
     }`
   ]
