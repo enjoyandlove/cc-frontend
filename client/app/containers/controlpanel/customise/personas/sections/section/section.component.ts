035551d4917dce75d82796394b08025d7df834b4
zacum <mfilgyhty@hotmail.com>
2018-06-25 13:29:09 -0400

    update rank

--- client/app/containers/controlpanel/customise/personas/sections/section/section.component.ts
+++ client/app/containers/controlpanel/customise/personas/sections/section/section.component.ts
@@ -1,6 +1,7 @@
 import { HttpParams } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { switchMap } from 'rxjs/operators';
 import { SNACKBAR_SHOW } from './../../../../../../reducers/snackbar.reducer';
 import { ICampusGuide, ITile } from './../../persona.interface';
 import { SectionsService } from './../sections.service';
@@ -18,16 +19,15 @@ export class PersonasSectionComponent implements OnInit {
   @Input() first: boolean;
   @Input() addSection: boolean;
   @Input() guide: ICampusGuide;
+  @Input() nextSection: ICampusGuide;
   @Input() previousSection: ICampusGuide;
-
-  // @Output() addSection: EventEmitter<null> = new EventEmitter();
 
   @Output() addTile: EventEmitter<null> = new EventEmitter();
   @Output() created: EventEmitter<ICampusGuide> = new EventEmitter();
   @Output() deleted: EventEmitter<number> = new EventEmitter();
 
   state = {
-    working: true
+    working: false
   };
 
   constructor(
@@ -44,8 +44,62 @@ export class PersonasSectionComponent implements OnInit {
     };
   }
 
+  moveUp() {
+    const school_id = this.session.g.get('school').id;
+    const [newRank, currentRank] = [this.previousSection.rank, this.guide.rank];
+    const currentTileBody = {
+      school_id,
+      rank: newRank
+    };
+    const pushedTileBody = {
+      school_id,
+      rank: currentRank
+    };
+    const updateCurrentTile$ = this.service.updateSectionTileCategory(
+      this.guide.id,
+      currentTileBody
+    );
+    const updatePushedTile$ = this.service.updateSectionTileCategory(
+      this.previousSection.id,
+      pushedTileBody
+    );
+
+    const stream$ = updateCurrentTile$.pipe(switchMap(() => updatePushedTile$));
+
+    stream$.subscribe((res) => console.log(res), (err) => console.log(err));
+  }
+
+  moveDown() {
+    const school_id = this.session.g.get('school').id;
+    const [newRank, currentRank] = [this.nextSection.rank, this.guide.rank];
+    const currentTileBody = {
+      school_id,
+      rank: newRank
+    };
+    const pushedTileBody = {
+      school_id,
+      rank: currentRank
+    };
+    const updateCurrentTile$ = this.service.updateSectionTileCategory(
+      this.guide.id,
+      currentTileBody
+    );
+    const updatePushedTile$ = this.service.updateSectionTileCategory(
+      this.nextSection.id,
+      pushedTileBody
+    );
+
+    const stream$ = updateCurrentTile$.pipe(switchMap(() => updatePushedTile$));
+
+    stream$.subscribe((res) => console.log(res), (err) => console.log(err));
+  }
+
   onMove(direction) {
-    console.log(direction, this.guide);
+    if (direction === 'up') {
+      this.moveUp();
+    } else {
+      this.moveDown();
+    }
   }
 
   setWorkingState(working) {
