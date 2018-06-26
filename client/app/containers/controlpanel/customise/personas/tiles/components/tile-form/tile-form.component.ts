10eb423ace687b476cde916f654db40678b5b43b
zacum <mfilgyhty@hotmail.com>
2018-06-26 11:58:21 -0400

    translate resource type

--- client/app/containers/controlpanel/customise/personas/tiles/components/tile-form/tile-form.component.ts
+++ client/app/containers/controlpanel/customise/personas/tiles/components/tile-form/tile-form.component.ts
@@ -1,3 +1,4 @@
+import { CPI18nService } from './../../../../../../../shared/services/i18n.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
@@ -9,11 +10,28 @@ import { FormGroup } from '@angular/forms';
 export class PersonasTileFormComponent implements OnInit {
   @Input() form: FormGroup;
 
+  resources;
+  contentTypes;
   extraFields = false;
-  resources = require('./resources.json');
-  contentTypes = require('./content-types.json');
 
-  constructor() {}
+  constructor(public cpI18n: CPI18nService) {}
 
-  ngOnInit(): void {}
+  onContentTypeChange(selected) {
+    console.log(selected);
+  }
+
+  ngOnInit(): void {
+    this.resources = require('./resources.json').map((resource) => {
+      return {
+        ...resource,
+        label: this.cpI18n.translate(resource.label)
+      };
+    });
+    this.contentTypes = require('./content-types.json').map((content) => {
+      return {
+        ...content,
+        label: this.cpI18n.translate(content.label)
+      };
+    });
+  }
 }
