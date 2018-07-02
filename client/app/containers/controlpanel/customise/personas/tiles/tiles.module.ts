52f39e4e88198bde05a769848d5a2cb98bafe1ef
zacum <mfilgyhty@hotmail.com>
2018-07-02 10:34:14 -0400

    rename stores.service from deals to dealsstores

--- client/app/containers/controlpanel/customise/personas/tiles/tiles.module.ts
+++ client/app/containers/controlpanel/customise/personas/tiles/tiles.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './../../../../../shared/shared.module';
-import { StoreService } from './../../../manage/deals/stores/store.service';
+import { StoreService } from './../../../../../shared/services/store.service';
 import {
   PersonasTileFormTypeSearchComponent,
   PersonasTileFormStoreListComponent,
