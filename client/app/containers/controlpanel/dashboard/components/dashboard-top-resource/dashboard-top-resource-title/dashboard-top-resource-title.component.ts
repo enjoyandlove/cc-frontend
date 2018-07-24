7e2d297d091f807252fe3bad433299621aa44f47
zacum <mfilgyhty@hotmail.com>
2018-07-24 17:28:39 +0500

    code refactored

--- client/app/containers/controlpanel/dashboard/components/dashboard-top-resource/dashboard-top-resource-title/dashboard-top-resource-title.component.ts
+++ client/app/containers/controlpanel/dashboard/components/dashboard-top-resource/dashboard-top-resource-title/dashboard-top-resource-title.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { CPSession } from '../../../../../../session';
-import { CP_PRIVILEGES_MAP } from '../../../../../../shared/constants';
-import { DashboardUtilsService } from '../../../dashboard.utils.service';
-import { canSchoolReadResource } from '../../../../../../shared/utils/privileges';
-
 @Component({
   selector: 'cp-dashboard-top-resource-title',
   templateUrl: './dashboard-top-resource-title.component.html',
@@ -13,21 +8,10 @@ import { canSchoolReadResource } from '../../../../../../shared/utils/privileges
 })
 export class DashboardTopResourceTitleComponent implements OnInit {
   @Input() item;
-  @Input() resource: string;
+  @Input() hasPrivilege;
+  @Input() resourceURL: string;
 
-  isSuperAdmin;
-  isOrientation;
-  canViewOrientation;
+  constructor(public router: Router) {}
 
-  constructor(
-    public router: Router,
-    public session: CPSession,
-    public helper: DashboardUtilsService
-  ) {}
-
-  ngOnInit() {
-    this.isOrientation = this.resource === 'orientation';
-    this.isSuperAdmin = this.helper.isSuperAdmin(this.session);
-    this.canViewOrientation = canSchoolReadResource(this.session.g, CP_PRIVILEGES_MAP.orientation);
-  }
+  ngOnInit() {}
 }
--- client/app/containers/controlpanel/dashboard/components/dashboard-top-resource/dashboard-top-resource.component.ts
+++ client/app/containers/controlpanel/dashboard/components/dashboard-top-resource/dashboard-top-resource.component.ts
@@ -4,6 +4,11 @@
  */
 import { Component, OnInit, Input } from '@angular/core';
 
+import { CPSession } from '../../../../../session';
+import { CP_PRIVILEGES_MAP } from '../../../../../shared/constants';
+import { DashboardUtilsService } from '../../dashboard.utils.service';
+import { canSchoolReadResource } from '../../../../../shared/utils/privileges';
+
 @Component({
   selector: 'cp-dashboard-top-resource',
   templateUrl: './dashboard-top-resource.component.html',
@@ -13,9 +18,38 @@ export class DashboardTopResourceComponent implements OnInit {
   @Input() items;
   @Input() resource: string;
 
+  isSuperAdmin;
+  isOrientation;
+  canViewOrientation;
+  hasPrivilege = false;
   defaultImage = require('public/default/user.png');
 
-  constructor() {}
+  constructor(
+    public session: CPSession,
+    public helper: DashboardUtilsService
+  ) {}
 
-  ngOnInit() {}
+  getResourceURL(item) {
+    if (this.isOrientation) {
+      return `/manage/${this.resource}/${item.calendar_id}/events/${item.id}`;
+    }
+
+    return `/manage/${this.resource}/${item.id}`;
+  }
+
+  ngOnInit() {
+    this.isOrientation = this.resource === 'orientation';
+    this.isSuperAdmin = this.helper.isSuperAdmin(this.session);
+    this.canViewOrientation = canSchoolReadResource(this.session.g, CP_PRIVILEGES_MAP.orientation);
+
+    if ((this.isSuperAdmin && !this.isOrientation)
+      || (this.isOrientation && this.canViewOrientation)) {
+
+      this.hasPrivilege = true;
+    } else if ((!this.isSuperAdmin && !this.isOrientation)
+      || (this.isOrientation && !this.canViewOrientation)) {
+
+      this.hasPrivilege = false;
+    }
+  }
 }
