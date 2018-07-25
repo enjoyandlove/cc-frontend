259b7fddfcbc651385023de2d5881e48ccc3e067
zacum <mfilgyhty@hotmail.com>
2018-07-25 10:56:12 -0400

    simple edit

--- client/app/containers/controlpanel/customise/personas/tiles/resources/components/resource-form/resource-form.component.ts
+++ client/app/containers/controlpanel/customise/personas/tiles/resources/components/resource-form/resource-form.component.ts
@@ -51,6 +51,7 @@ export class PersonasResourceFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.form.value);
     this.form.valueChanges.subscribe(() => {
       this.formChange.emit(this.form);
     });
