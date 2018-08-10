0fbecdb593a40176411685097f906de5bf9dd5d3
zacum <mfilgyhty@hotmail.com>
2018-08-10 15:15:38 +0000

    Feature/web 2074 send attendees message

--- client/app/containers/controlpanel/manage/events/attendance/components/events-attendance-action-box/events-attendance-action-box.component.ts
+++ client/app/containers/controlpanel/manage/events/attendance/components/events-attendance-action-box/events-attendance-action-box.component.ts
@@ -1,7 +1,10 @@
+/*tslint:disable:max-line-length*/
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/index';
 
 import { CPSession } from './../../../../../../../session';
 import { EventUtilService } from '../../../events.utils.service';
+import { CPI18nService } from '../../../../../../../shared/services';
 import { CP_PRIVILEGES_MAP } from './../../../../../../../shared/constants/privileges';
 import { canSchoolWriteResource } from './../../../../../../../shared/utils/privileges/privileges';
 
@@ -12,15 +15,24 @@ import { canSchoolWriteResource } from './../../../../../../../shared/utils/priv
 })
 export class EventsAttendanceActionBoxComponent implements OnInit {
   @Input() event: any;
+  @Input() totalAttendees = new BehaviorSubject(null);
   @Input() isOrientation: boolean;
 
   @Output() querySearch: EventEmitter<string> = new EventEmitter();
   @Output() createExcel: EventEmitter<null> = new EventEmitter();
+  @Output() sendMessage: EventEmitter<null> = new EventEmitter();
 
   eventCheckinRoute;
-  canDownload = false;
+  disableMessageAttendees;
+  messageAttendeesTooltipText;
+  canDownload = canSchoolWriteResource(this.session.g, CP_PRIVILEGES_MAP.event_attendance);
+  canMessage = canSchoolWriteResource(this.session.g, CP_PRIVILEGES_MAP.campus_announcements);
 
-  constructor(public session: CPSession, public utils: EventUtilService) {}
+  constructor(
+    public session: CPSession,
+    public cpI18n: CPI18nService,
+    public utils: EventUtilService
+  ) {}
 
   onSearch(query) {
     this.querySearch.emit(query);
@@ -30,8 +42,27 @@ export class EventsAttendanceActionBoxComponent implements OnInit {
     this.createExcel.emit();
   }
 
+  sendAttendeesMessage() {
+    if (this.disableMessageAttendees) {
+      return;
+    }
+
+    this.sendMessage.emit();
+  }
+
   ngOnInit() {
     this.eventCheckinRoute = this.utils.getEventCheckInLink(this.isOrientation);
-    this.canDownload = canSchoolWriteResource(this.session.g, CP_PRIVILEGES_MAP.event_attendance);
+
+    this.totalAttendees.subscribe((attendees) => {
+      this.disableMessageAttendees = !this.canMessage || !attendees;
+      if (!attendees) {
+        this.messageAttendeesTooltipText = this.cpI18n.translate('t_events_attendance_no_attendees_tooltip_text');
+      } else if (!this.canMessage) {
+        this.messageAttendeesTooltipText = this.cpI18n.translate('t_events_attendance_no_permission_tooltip_text');
+      } else {
+        this.messageAttendeesTooltipText = '';
+      }
+    });
+
   }
 }
--- client/app/containers/controlpanel/manage/events/attendance/events-attendance.component.ts
+++ client/app/containers/controlpanel/manage/events/attendance/events-attendance.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpParams } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/index';
 import { Store } from '@ngrx/store';
 
 import { CheckInMethod } from '../event.status';
@@ -10,10 +11,13 @@ import { FORMAT } from '../../../../../shared/pipes';
 import { EventUtilService } from './../events.utils.service';
 import { CPDate } from '../../../../../shared/utils/date/date';
 import { BaseComponent } from '../../../../../base/base.component';
+import { CP_PRIVILEGES_MAP } from '../../../../../shared/constants';
+import { SNACKBAR_SHOW } from '../../../../../reducers/snackbar.reducer';
 import { createSpreadSheet } from '../../../../../shared/utils/csv/parser';
 import { amplitudeEvents } from '../../../../../shared/constants/analytics';
 import { IHeader, HEADER_UPDATE } from '../../../../../reducers/header.reducer';
-import { CPI18nService, CPTrackingService } from '../../../../../shared/services';
+import { canSchoolWriteResource } from '../../../../../shared/utils/privileges/privileges';
+import { CPI18nService, CPTrackingService, RouteLevel } from '../../../../../shared/services';
 
 interface IState {
   sort_field: string;
@@ -43,21 +47,27 @@ export class EventsAttendanceComponent extends BaseComponent implements OnInit {
 
   event;
   urlPrefix;
-  attendees;
+  messageData;
   sortingLabels;
+  attendees = [];
   loading = true;
   eventId: number;
+  allStudents = false;
   state: IState = state;
   attendeesLoading = true;
   downloadEventProperties;
+  isSendMessageModal = false;
+  messageAttendeesTooltipText;
   dateFormat = FORMAT.DATETIME;
+  totalAttendees = new BehaviorSubject(null);
+  appCheckIn = CheckInMethod.app;
+  canMessage = canSchoolWriteResource(this.session.g, CP_PRIVILEGES_MAP.campus_announcements);
 
   eventProperties = {
-    data_type: null,
-    event_id: null,
-    start_date: null,
-    end_date: null,
-    location: null
+    host_type: null,
+    sub_menu_name: null,
+    engagement_type: null,
+    announcement_source: null
   };
 
   constructor(
@@ -136,7 +146,19 @@ export class EventsAttendanceComponent extends BaseComponent implements OnInit {
 
     super
       .fetchData(stream$)
-      .then((res) => (this.attendees = res.data))
+      .then((res) => {
+        this.attendees = res.data;
+        this.totalAttendees.next(res.data.length);
+        setTimeout(
+          () => {
+            $(function () {
+              $('[data-toggle="tooltip"]').tooltip();
+            });
+          },
+
+          10
+        );
+      })
       .catch((_) => {});
   }
 
@@ -242,6 +264,82 @@ export class EventsAttendanceComponent extends BaseComponent implements OnInit {
     );
   }
 
+  onFlashMessage(data) {
+    this.trackSendMessageEvents(data.hostType);
+
+    this.store.dispatch({
+      type: SNACKBAR_SHOW,
+      payload: {
+        body: this.cpI18n.translate('announcement_success_sent'),
+        autoClose: true
+      }
+    });
+  }
+
+  onComposeTeardown() {
+    $('#sendMessageModal').modal('hide');
+    this.messageData = null;
+    this.isSendMessageModal = false;
+  }
+
+  messageAllAttendees() {
+    this.allStudents = true;
+
+    const userIds = this.attendees.filter((attendee) => attendee.user_id)
+      .map((attendee) => attendee.user_id);
+
+    this.messageData = {
+      name: this.event.title,
+      userIds
+    };
+
+    this.loadModal();
+  }
+
+  messageAttendee(attendee) {
+    if (!this.canMessage) {
+      return;
+    }
+
+    this.allStudents = false;
+
+    this.messageData = {
+      name: `${attendee.firstname} ${attendee.lastname}`,
+      userIds: [attendee.user_id]
+    };
+
+    this.loadModal();
+  }
+
+  loadModal() {
+    this.isSendMessageModal = true;
+    setTimeout(
+      () => {
+        $('#sendMessageModal').modal();
+      },
+
+      1
+    );
+  }
+
+  trackSendMessageEvents(host_type) {
+    const engagement_type = this.allStudents
+      ? amplitudeEvents.ALL_STUDENTS
+      : amplitudeEvents.SINGLE_STUDENT;
+
+    this.eventProperties = {
+      ...this.eventProperties,
+      host_type,
+      engagement_type,
+      announcement_source: amplitudeEvents.EVENT_ASSESSMENT,
+      sub_menu_name: this.cpTracking.activatedRoute(RouteLevel.second)
+    };
+
+    this.cpTracking.amplitudeEmitEvent(
+      amplitudeEvents.MENAGE_SENT_MESSAGE,
+      this.eventProperties);
+  }
+
   ngOnInit() {
     this.urlPrefix = this.utils.buildUrlPrefix(
       this.clubId,
@@ -255,6 +353,10 @@ export class EventsAttendanceComponent extends BaseComponent implements OnInit {
       name: this.cpI18n.translate('attendee'),
       method: this.cpI18n.translate('events_checked_in_method')
     };
+
+    this.messageAttendeesTooltipText = !this.canMessage
+      ? this.cpI18n.translate('t_events_attendance_no_permission_tooltip_text')
+      : '';
 
     this.fetch();
   }
