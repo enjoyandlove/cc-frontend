d2840f609ac484b2e1988abdfcb5357f78632545
zacum <mfilgyhty@hotmail.com>
2018-08-21 20:01:01 +0000

    more test

---
+++ client/app/containers/controlpanel/customise/personas/tiles/__mock__/index.ts
@@ -0,0 +1,17 @@
+import { ITile } from './../tile.interface';
+
+export const mockTile: ITile = {
+  id: 1,
+  name: 'Mock Tile',
+  school_persona_id: 1,
+  tile_category_id: 1,
+  description: '',
+  rank: 1,
+  featured_rank: -1,
+  color: 'FFFFFF',
+  img_url: 'img_url',
+  type: 8,
+  extra_info: {},
+  visibility_status: 1,
+  related_link_data: {}
+};
