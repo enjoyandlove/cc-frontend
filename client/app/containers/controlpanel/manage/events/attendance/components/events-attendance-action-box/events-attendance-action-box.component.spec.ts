700dce9b54206d1a8244063775ac68a636d7f8ef
zacum <mfilgyhty@hotmail.com>
2018-08-30 16:47:59 +0500

    attendee message bugs fixed

--- client/app/containers/controlpanel/manage/events/attendance/components/events-attendance-action-box/events-attendance-action-box.component.spec.ts
+++ client/app/containers/controlpanel/manage/events/attendance/components/events-attendance-action-box/events-attendance-action-box.component.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/index';
 
 import { EventsModule } from '../../../events.module';
+import { CheckInMethod } from '../../../event.status';
 import { CPSession } from '../../../../../../../session';
 import { EventUtilService } from '../../../events.utils.service';
 import { mockUser } from '../../../../../../../session/mock/user';
@@ -11,7 +12,9 @@ import { mockSchool } from '../../../../../../../session/mock/school';
 import { CPI18nService } from '../../../../../../../shared/services';
 import { EventsAttendanceActionBoxComponent } from './events-attendance-action-box.component';
 
-fdescribe('EventsAttendanceActionBoxComponent', () => {
+const mockAttendees = require('../../../__mock__/eventAttendees.json');
+
+describe('EventsAttendanceActionBoxComponent', () => {
   let component: EventsAttendanceActionBoxComponent;
   let fixture: ComponentFixture<EventsAttendanceActionBoxComponent>;
 
@@ -37,19 +40,46 @@ fdescribe('EventsAttendanceActionBoxComponent', () => {
     })
   );
 
-  it('should have canMessage privileges', () => {
-    component.ngOnInit();
-    // expect(component.canMessage).toBeTruthy();
-
-    /*component.session.g.set('user', { school_level_privileges: {} });
-    component.ngOnInit();
-    expect(component.canMessage).toBeFalsy();*/
-  });
-
-  it('onTabClick - existing store', () => {
-    component.updateQrCode.next([1, 2, 3]);
+  it('toggle updateQrCode', () => {
+    const enableQrLabel = component.cpI18n.translate('t_events_assessment_disable_qr_check_in');
+    const disableQrLabel = component.cpI18n.translate('t_events_assessment_enable_qr_check_in');
+    component.updateQrCode.next([CheckInMethod.web, CheckInMethod.app, CheckInMethod.webQr]);
 
     component.ngOnInit();
     expect(component.hasQr).toBeTruthy();
+    expect(component.qrLabel).toBe(enableQrLabel);
+
+    component.updateQrCode.next([CheckInMethod.web, CheckInMethod.webQr]);
+
+    component.ngOnInit();
+    expect(component.hasQr).toBeFalsy();
+    expect(component.qrLabel).toBe(disableQrLabel);
+  });
+
+  it('disable canMessage permission', () => {
+    component.totalAttendees.next(mockAttendees);
+    component.session.g.set('user', { school_level_privileges: {} });
+
+    const noPermissionTooltip = component.cpI18n.
+    translate('t_events_attendance_no_permission_tooltip_text');
+
+    component.ngOnInit();
+
+    expect(component.canMessage).toBeFalsy();
+    expect(component.disableMessageAttendees).toBeTruthy();
+    expect(component.messageAttendeesTooltipText).toBe(noPermissionTooltip);
+  });
+
+  it('no students to message', () => {
+    component.totalAttendees.next(mockAttendees);
+
+    const noStudentTooltip = component.cpI18n.
+    translate('t_events_attendance_no_students_tooltip_text');
+
+    component.ngOnInit();
+
+    expect(component.canMessage).toBeTruthy();
+    expect(component.disableMessageAttendees).toBeTruthy();
+    expect(component.messageAttendeesTooltipText).toBe(noStudentTooltip);
   });
 });
