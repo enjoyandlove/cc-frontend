5289534e88f007c8580f6ff9f6b2e5c6e99f12d2
zacum <mfilgyhty@hotmail.com>
2018-09-06 20:10:37 +0500

    unit test added

--- client/app/shared/utils/date/date.spec.ts
+++ client/app/shared/utils/date/date.spec.ts
@@ -1,7 +1,9 @@
 import { CPDate } from './date';
 
-const dateStr = '2018-03-29T14:22:17.592Z';
+const mockDuration = 1301418;
 const mockEpochDate = 1522347737;
+const durationFormat = 'd hh:mm:ss';
+const dateStr = '2018-03-29T14:22:17.592Z';
 
 describe('CPDate', () => {
   it('Should convert a valid Date object to epoch time', () => {
@@ -12,5 +14,9 @@ describe('CPDate', () => {
     expect(CPDate.fromEpoch(mockEpochDate, 'America/Toronto').toISOString()).toEqual(
       '2018-03-29T18:22:17.000Z'
     );
+  });
+
+  it('Should convert time duration/seconds to comprehensive format', () => {
+      expect(CPDate.getTimeDuration(mockDuration).format(durationFormat)).toEqual('15 01:30:18');
   });
 });
