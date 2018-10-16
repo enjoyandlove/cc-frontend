b317cc0237da3909016fdf7d7a656474de2e1a34
zacum <mfilgyhty@hotmail.com>
2018-10-16 15:52:41 -0400

    demo test using configureTestModule

---
+++ client/app/containers/controlpanel/assess/engagement/components/engagement-stats/engagement-stats.component.spec.ts
@@ -0,0 +1,56 @@
+import { CPI18nService } from './../../../../../../shared/services/i18n.service';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+import { EngagementModule } from './../../engagement.module';
+import { EngagementStatsComponent } from './engagement-stats.component';
+
+import { configureTestSuite } from '../../../../../../shared/tests';
+
+fdescribe('EngagementStatsComponent', () => {
+  configureTestSuite();
+
+  beforeAll((done) =>
+    (async () => {
+      TestBed.configureTestingModule({
+        imports: [EngagementModule],
+        providers: [CPI18nService]
+      });
+
+      await TestBed.compileComponents();
+    })()
+      .then(done)
+      .catch(done.fail)
+  );
+
+  describe('a', () => {
+    let component: EngagementStatsComponent;
+    let fixture: ComponentFixture<EngagementStatsComponent>;
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(EngagementStatsComponent);
+      component = fixture.componentInstance;
+    });
+
+    it('should init', () => {
+      expect(component).toBeTruthy();
+    });
+    it('should init', () => {
+      expect(component).toBeTruthy();
+    });
+    it('should init', () => {
+      expect(component).toBeTruthy();
+    });
+    it('should init', () => {
+      expect(component).toBeTruthy();
+    });
+    it('should init', () => {
+      expect(component).toBeTruthy();
+    });
+    it('should init', () => {
+      expect(component).toBeTruthy();
+    });
+    it('should init', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+});
