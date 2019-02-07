d1ad0364b75c8c5524ded93d8ba39dca89347b4b
zacum <mfilgyhty@hotmail.com>
2019-02-07 13:42:19 +0500

    added mock data

--- client/app/containers/controlpanel/manage/locations/categories/tests/mock.ts
+++ client/app/containers/controlpanel/manage/locations/categories/tests/mock.ts
@@ -13,20 +13,24 @@ export const filledForm = {
 export const mockCategories = [
   {
     id: 123,
+    color: 'FFA416',
     category_type: 1,
     is_default: true,
     img_url: 'img.jpeg',
     name: 'Hello world',
     category_type_id: 1,
+    locations_count: 1,
     category_type_name: 'dining'
   },
   {
     id: 1234,
+    color: 'CF0000',
     category_type: 4,
     is_default: true,
     img_url: 'img.jpeg',
     name: 'Hello world',
     category_type_id: 2,
+    locations_count: 0,
     category_type_name: 'dining'
   }
 ];
