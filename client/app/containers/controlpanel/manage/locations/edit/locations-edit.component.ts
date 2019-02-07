31af7363ff98acd466fb4713db3e92a77fb3d54f
zacum <mfilgyhty@hotmail.com>
2019-02-07 18:40:44 +0500

    code refactored

--- client/app/containers/controlpanel/manage/locations/edit/locations-edit.component.ts
+++ client/app/containers/controlpanel/manage/locations/edit/locations-edit.component.ts
@@ -77,7 +77,6 @@ export class LocationsEditComponent extends BaseComponent implements OnInit, OnD
     };
 
     this.store.dispatch(new fromStore.EditLocation(payload));
-    this.updateCategoryInfo(this.locationForm.get('category_id').value);
   }
 
   buildHeader() {
@@ -181,24 +180,6 @@ export class LocationsEditComponent extends BaseComponent implements OnInit, OnD
         return parsedCategories;
       })
     );
-  }
-
-  updateCategoryInfo(categoryId) {
-    this.store.select(fromCategoryStore.getCategories).pipe(
-      takeUntil(this.destroy$)
-    ).subscribe((categories: ICategory[]) => {
-      categories.map((category: ICategory) => {
-        if (category.id === this.categoryId) {
-          category['locations_count'] = category['locations_count'] - 1;
-        }
-
-        if (category.id === categoryId) {
-          category['locations_count'] = category['locations_count'] + 1;
-        }
-
-        return category;
-      });
-    });
   }
 
   ngOnInit() {
