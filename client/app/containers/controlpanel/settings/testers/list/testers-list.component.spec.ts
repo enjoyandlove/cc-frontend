5bd82251b06b59a6eea0077ca9e913f574c2ee72
zacum <mfilgyhty@hotmail.com>
2019-02-28 15:40:18 -0500

    Add tests for list component

--- client/app/containers/controlpanel/settings/testers/list/testers-list.component.spec.ts
+++ client/app/containers/controlpanel/settings/testers/list/testers-list.component.spec.ts
@@ -1,25 +1,69 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 
+import { reducerMap } from '../store';
+import { CPI18nService } from '@shared/services';
+import { configureTestSuite } from '@shared/tests';
+import * as actions from '../store/testers.actions';
+import { SharedModule } from '@shared/shared.module';
+import { SETTINGS_TESTERS, SORT_DIRECTION } from '@shared/constants';
 import { TestersListComponent } from './testers-list.component';
+import { TestUsersComponent } from './components/test-users/test-users.component';
+import { TestersActionBoxComponent } from './components/testers-action-box/testers-action-box.component';
 
 describe('TestersListComponent', () => {
-  let component: TestersListComponent;
-  let fixture: ComponentFixture<TestersListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TestersListComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestersListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  configureTestSuite();
+  beforeAll((done) => {
+    (async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          SharedModule,
+          StoreModule.forRoot({}),
+          StoreModule.forFeature(SETTINGS_TESTERS, reducerMap)
+        ],
+        providers: [CPI18nService],
+        declarations: [TestersListComponent, TestersActionBoxComponent, TestUsersComponent]
+      }).compileComponents();
+    })()
+      .then(done)
+      .catch(done.fail);
   });
 
-  it('should create', () => {
+  let component: TestersListComponent;
+  let fixture: ComponentFixture<TestersListComponent>;
+  let spyFetch;
+  let spyDispatch;
+
+  beforeEach(
+    async(() => {
+      fixture = TestBed.createComponent(TestersListComponent);
+      component = fixture.componentInstance;
+
+      spyFetch = spyOn(component, 'fetch').and.callThrough();
+      spyDispatch = spyOn(component.store, 'dispatch');
+
+      fixture.detectChanges();
+    })
+  );
+
+  it('should create and fetch w/ dispatch', () => {
     expect(component).toBeTruthy();
+    expect(spyFetch).toHaveBeenCalled();
+    expect(spyDispatch).toHaveBeenCalledWith(new actions.LoadTesters());
+  });
+
+  it('should search and fetch', () => {
+    spyFetch.calls.reset();
+    const search = 'test';
+    component.doSearch(search);
+    expect(spyDispatch).toHaveBeenCalledWith(new actions.SetTestersSearch(search));
+    expect(spyFetch).toHaveBeenCalled();
+  });
+
+  it('should toggle sort and fetch', () => {
+    spyFetch.calls.reset();
+    component.doSort(SORT_DIRECTION.ASC);
+    expect(spyDispatch).toHaveBeenCalledWith(new actions.SetTestersSort(SORT_DIRECTION.DESC));
+    expect(spyFetch).toHaveBeenCalled();
   });
 });
