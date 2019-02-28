b0ec9865e9d0d697b8b6fa210e860daad54ce089
zacum <mfilgyhty@hotmail.com>
2019-02-28 14:48:53 -0500

    Test dumb components

--- client/app/containers/controlpanel/settings/testers/list/components/test-users/test-users.component.spec.ts
+++ client/app/containers/controlpanel/settings/testers/list/components/test-users/test-users.component.spec.ts
@@ -1,23 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestUsersComponent } from './test-users.component';
+import { configureTestSuite } from '@client/app/shared/tests';
+import { SharedModule } from '@client/app/shared/shared.module';
+import { CPI18nService } from '@client/app/shared/services';
 
-describe('TestUsersComponent', () => {
+fdescribe('TestUsersComponent', () => {
+  configureTestSuite();
+  beforeAll((done) => {
+    (async () => {
+      await TestBed.configureTestingModule({
+        imports: [SharedModule],
+        providers: [CPI18nService],
+        declarations: [TestUsersComponent]
+      }).compileComponents();
+    })()
+      .then(done)
+      .catch(done.fail);
+  });
+
   let component: TestUsersComponent;
   let fixture: ComponentFixture<TestUsersComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TestUsersComponent ]
+  beforeEach(
+    async(() => {
+      fixture = TestBed.createComponent(TestUsersComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestUsersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- client/app/containers/controlpanel/settings/testers/list/components/testers-action-box/testers-action-box.component.spec.ts
+++ client/app/containers/controlpanel/settings/testers/list/components/testers-action-box/testers-action-box.component.spec.ts
@@ -1,23 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestersActionBoxComponent } from './testers-action-box.component';
+import { configureTestSuite } from '@client/app/shared/tests';
+import { SharedModule } from '@client/app/shared/shared.module';
+import { CPI18nService } from '@client/app/shared/services';
 
-describe('TestersActionBoxComponent', () => {
+fdescribe('TestersActionBoxComponent', () => {
+  configureTestSuite();
+  beforeAll((done) => {
+    (async () => {
+      await TestBed.configureTestingModule({
+        imports: [SharedModule],
+        providers: [CPI18nService],
+        declarations: [TestersActionBoxComponent]
+      }).compileComponents();
+    })()
+      .then(done)
+      .catch(done.fail);
+  });
+
   let component: TestersActionBoxComponent;
   let fixture: ComponentFixture<TestersActionBoxComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TestersActionBoxComponent ]
+  beforeEach(
+    async(() => {
+      fixture = TestBed.createComponent(TestersActionBoxComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestersActionBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
