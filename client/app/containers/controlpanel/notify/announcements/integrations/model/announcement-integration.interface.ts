f3ee5a652fcdec5e6699d54ebda1d52bdf1ebb70
zacum <mfilgyhty@hotmail.com>
2019-03-14 19:41:05 +0000

    Revert "Actions, effects, reducers for creating announcement integrations"
    This reverts commit ef2483ec47cddc07fe94b1dc655d59a729d2edac.

--- client/app/containers/controlpanel/notify/announcements/integrations/model/announcement-integration.interface.ts
+++ client/app/containers/controlpanel/notify/announcements/integrations/model/announcement-integration.interface.ts
@@ -1,7 +1,7 @@
 import { IFeedIntegration } from '@libs/integrations/common/model';
 import { AnnouncementPriority } from './../../announcements.interface';
 
-export interface IAnnoucementsIntegration extends IFeedIntegration {
+export interface IAnnouncementsIntegration extends IFeedIntegration {
   store_id: number;
   priority: AnnouncementPriority;
 }
