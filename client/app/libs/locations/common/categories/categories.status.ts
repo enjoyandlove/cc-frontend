bfa4e10116660ab5eccf5d2d0d0022da5bc4110b
zacum <mfilgyhty@hotmail.com>
2019-03-15 14:52:56 +0500

    moved icon types to utils

--- client/app/libs/locations/common/categories/categories.status.ts
+++ client/app/libs/locations/common/categories/categories.status.ts
@@ -1,5 +1,3 @@
-const dbPath = 'https://d38h7mnlv8qddx.cloudfront.net/';
-
 export interface ICategoriesApiQuery {
   search_str: string;
   sort_field: string;
@@ -21,17 +19,3 @@ export enum LocationCategoryLocale {
   'eng' = 'en',
   'fr' = 'fr'
 }
-
-export const categoryIconType = {
-  [dbPath + 'location_pin.png']: 'Pin',
-  [dbPath + 'location_category_bus.png']: 'Bus',
-  [dbPath + 'location_category_bike.png']: 'Bike',
-  [dbPath + 'location_category_dining.png']: 'Food',
-  [dbPath + 'location_category_library.png']: 'Book',
-  [dbPath + 'location_category_health.png']: 'Health',
-  [dbPath + 'location_category_security.png']: 'Badge',
-  [dbPath + 'location_category_parking.png']: 'Parking',
-  [dbPath + 'location_category_housing.png']: 'Building',
-  [dbPath + 'location_category_information.png']: 'Information',
-  [dbPath + 'location_category_accessability.png']: 'Accessibility',
-};
