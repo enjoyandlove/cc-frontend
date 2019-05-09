3c067d58bf0b23b601637c83c02c633e4d55fffa
zacum <mfilgyhty@hotmail.com>
2019-05-09 18:28:08 +0000

    update tests

--- libraries/ui/karma.conf.js
+++ libraries/ui/karma.conf.js
@@ -7,8 +7,8 @@ module.exports = function(config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-mocha-reporter'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
