b6f7d063dc0bc6e47c10648dc0cbe9a88af23604
zacum <mfilgyhty@hotmail.com>
2019-05-14 15:44:00 +0000

    Feature/enable announcement int

--- client/environments/environment.prod.usa.ts
+++ client/environments/environment.prod.usa.ts
@@ -3,11 +3,7 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   root: '/dist/',
   production: true,
-  flags: {
-    ANNOUNCEMENT_INTEGRATION: {
-      whitelist: []
-    }
-  },
+  flags: {},
   envName: 'production_usa',
   keys: {
     sentryDsn: 'https://0b6c76a5691d4b7399394aa79753acef@sentry.io/207033'
