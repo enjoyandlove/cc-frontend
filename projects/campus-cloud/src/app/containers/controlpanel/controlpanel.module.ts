42c828932cc42bf978473c3811ba8216ba564460
zacum <mfilgyhty@hotmail.com>
2019-08-09 21:02:26 +0000

    Apply suggestion to projects/campus-cloud/src/app/containers/controlpanel/customise/personas/tiles/resources/components/resource-list-form/resource-list-form.component.ts

--- projects/campus-cloud/src/app/containers/controlpanel/controlpanel.module.ts
+++ projects/campus-cloud/src/app/containers/controlpanel/controlpanel.module.ts
@@ -1,14 +1,13 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
-import { AdminService, SchoolService, StoreService } from '../../shared/services';
 import { SharedModule } from '../../shared/shared.module';
-
+import { ToastModule } from '../../ready/toast/toast.module';
+import { ControlPanelService } from './controlpanel.service';
 import { ControlPanelComponent } from './controlpanel.component';
 import { ControlPanelRoutingModule } from './controlpanel.routing.module';
-import { ControlPanelService } from './controlpanel.service';
-import { ToastModule } from '../../ready/toast/toast.module';
+import { AdminService, SchoolService, StoreService } from '../../shared/services';
 
 @NgModule({
   declarations: [ControlPanelComponent],
