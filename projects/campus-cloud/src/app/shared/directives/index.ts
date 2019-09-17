323c424435d1c96d4e49741182a0bddb479e0d0c
zacum <mfilgyhty@hotmail.com>
2019-09-17 20:14:22 +0000

    Feature/web 4059 refactor quill

--- projects/campus-cloud/src/app/shared/directives/index.ts
+++ projects/campus-cloud/src/app/shared/directives/index.ts
@@ -2,6 +2,7 @@ export * from './cp-host';
 export * from './tracking';
 export * from './cp-target';
 export * from './cp-tooltip';
+export * from './text-editor';
 export * from './cp-lightbox';
 export * from './color-picker';
 export * from './feature-toggle';
