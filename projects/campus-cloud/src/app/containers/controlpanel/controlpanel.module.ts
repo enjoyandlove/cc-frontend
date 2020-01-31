9815bac2852e3d516ff6418775a746e68cdc6884
zacum <mfilgyhty@hotmail.com>
2020-01-31 16:38:10 +0000

    feat(onboarding): remove What's New banner and Beamer request

--- projects/campus-cloud/src/app/containers/controlpanel/controlpanel.module.ts
+++ projects/campus-cloud/src/app/containers/controlpanel/controlpanel.module.ts
@@ -1,23 +1,15 @@
-import { ToastModule } from '@ready-education/ready-ui/feedback-and-indicators';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../../shared/shared.module';
-import { ControlPanelService } from './controlpanel.service';
 import { ControlPanelComponent } from './controlpanel.component';
 import { ControlPanelRoutingModule } from './controlpanel.routing.module';
 import { AdminService, SchoolService, StoreService } from '../../shared/services';
 
 @NgModule({
   declarations: [ControlPanelComponent],
-  imports: [
-    RouterModule,
-    ControlPanelRoutingModule,
-    CommonModule,
-    SharedModule,
-    ToastModule.forRoot()
-  ],
-  providers: [ControlPanelService, AdminService, StoreService, SchoolService]
+  imports: [RouterModule, ControlPanelRoutingModule, CommonModule, SharedModule],
+  providers: [AdminService, StoreService, SchoolService]
 })
 export class ControlPanelModule {}
