3bbf2f3cc05c2883f1ef3961077989dcceaf7ac5
zacum <mfilgyhty@hotmail.com>
2020-04-13 20:34:10 +0000

    Ready ui ghosts

---
+++ projects/ready-ui/src/lib/structure/skeleton/skeleton-avatar/skeleton-avatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ready-ui-skeleton-avatar',
+  templateUrl: './skeleton-avatar.component.html',
+  styleUrls: ['./skeleton-avatar.component.scss']
+})
+export class SkeletonAvatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
