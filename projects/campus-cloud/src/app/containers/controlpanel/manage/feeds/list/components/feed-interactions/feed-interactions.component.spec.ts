74f9264150f42821f708313eb1f9067e43aa534c
zacum <mfilgyhty@hotmail.com>
2020-05-04 20:32:51 +0000

    Apply suggestion to projects/campus-cloud/src/app/containers/controlpanel/customise/personas/tiles/resources/components/resource-list-form/resource-list-form.component.ts

--- projects/campus-cloud/src/app/containers/controlpanel/manage/feeds/list/components/feed-interactions/feed-interactions.component.spec.ts
+++ projects/campus-cloud/src/app/containers/controlpanel/manage/feeds/list/components/feed-interactions/feed-interactions.component.spec.ts
@@ -1,9 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ModalService } from '@ready-education/ready-ui/overlays';
+import { provideMockStore } from '@ngrx/store/testing';
+import { HttpParams } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { CPSession } from '@campus-cloud/session';
 import { mockSchool } from '@campus-cloud/session/mock';
 import { CPTestModule } from '@campus-cloud/shared/tests';
+import { mockFeed } from '@controlpanel/manage/feeds/tests';
+import { InteractionLikeType } from '@campus-cloud/services';
 import { FeedInteractionsComponent } from './feed-interactions.component';
 
 describe('FeedInteractionsComponent', () => {
@@ -14,7 +19,16 @@ describe('FeedInteractionsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [CPTestModule],
-      providers: [ModalService],
+      providers: [
+        ModalService,
+        provideMockStore({
+          initialState: {
+            WALLS_STATE: {
+              feeds: {}
+            }
+          }
+        })
+      ],
       declarations: [FeedInteractionsComponent]
     }).compileComponents();
   }));
@@ -22,13 +36,38 @@ describe('FeedInteractionsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FeedInteractionsComponent);
     component = fixture.componentInstance;
+    component.feed = mockFeed;
 
     session = TestBed.inject(CPSession);
     session.g.set('school', mockSchool);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('fetch', () => {
+    let spy: jasmine.Spy;
+
+    beforeEach(() => {
+      spy = spyOn(component.service, 'get');
+    });
+
+    it('it should have right params', (done) => {
+      spy.and.returnValue(of([]));
+      component.likeType = InteractionLikeType.like;
+      fixture.detectChanges();
+
+      const sub = component.fetch();
+
+      sub.subscribe(() => {
+        const [_, endRange, params] = spy.calls.mostRecent().args as [number, number, HttpParams];
+        expect(endRange).toBe(5);
+        const acceptedParams = ['content_id', 'like_type', 'content_type', 'school_id', 'group_id'];
+        acceptedParams.forEach((p) =>
+          expect(params.get(p)).toBeDefined(`missing expected param ${p}`)
+        );
+        expect(params.get('content_id')).toBe(component.feed.id.toString());
+        expect(params.get('like_type')).toBe(component.likeType.toString());
+        done();
+      });
+    });
   });
 });
