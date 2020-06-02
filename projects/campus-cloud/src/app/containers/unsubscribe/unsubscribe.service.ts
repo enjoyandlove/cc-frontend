6bbf7701283940db474974be809e1a4850a05c58
zacum <mfilgyhty@hotmail.com>
2020-06-02 13:08:53 +0500

    added cc token in header for external link

---
+++ projects/campus-cloud/src/app/containers/unsubscribe/unsubscribe.service.ts
@@ -0,0 +1,22 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { EnvService } from '@campus-cloud/config/env';
+import { ApiService } from '@campus-cloud/base/services';
+
+@Injectable({ providedIn: 'root' })
+export class UnsubscribeService extends ApiService {
+  constructor(http: HttpClient, router: Router, env: EnvService) {
+    super(http, router, env);
+  }
+
+  getHeaders() {
+    const auth = `CCToke ${this.KEY}`;
+
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: auth
+    });
+  }
+}
