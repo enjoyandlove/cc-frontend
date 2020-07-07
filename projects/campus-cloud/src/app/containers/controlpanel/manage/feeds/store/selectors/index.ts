15c52946184083ce08bdee37f3552cc0ee6a81e2
zacum <mfilgyhty@hotmail.com>
2020-07-07 22:57:16 +0000

    do not apply filters on threads when opening detail page, do not override comment_count in detail page so that it matches app behavior, do not display input box for flagged post

--- projects/campus-cloud/src/app/containers/controlpanel/manage/feeds/store/selectors/index.ts
+++ projects/campus-cloud/src/app/containers/controlpanel/manage/feeds/store/selectors/index.ts
@@ -68,6 +68,13 @@ export const getViewFilters = createSelector(
   })
 );
 
+export const getThreadById = (thread_id: number) => createSelector(
+  getFeedsState,
+  ({ threads }) => {
+    return threads.find((thread) => thread.id === thread_id);
+  }
+);
+
 export const getThreads = createSelector(
   getFeedsState,
   getViewFilters,
