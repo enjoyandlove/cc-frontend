2120ad4dd6d773bc18676b9b924d8799ae6d4cd3
zacum <mfilgyhty@hotmail.com>
2020-08-19 01:28:50 +0000

    feature/FES/6819/main_community_page_comments_permalink

--- projects/campus-cloud/src/app/containers/controlpanel/manage/feeds/list/components/feed-input-box/feed-input-box.component.ts
+++ projects/campus-cloud/src/app/containers/controlpanel/manage/feeds/list/components/feed-input-box/feed-input-box.component.ts
@@ -57,6 +57,7 @@ export class FeedInputBoxComponent implements OnInit, OnDestroy {
 
   @Input() replyView: boolean;
   @Input() feed: ICampusThread | ISocialGroupThread;
+  @Input() isDefaultView: boolean;
 
   @Output() created: EventEmitter<null> = new EventEmitter();
 
